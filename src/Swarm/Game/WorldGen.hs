f29bd2a9f3db54ffb946e57b6de194d42770c5f3
Brent Yorgey <byorgey@gmail.com>
2021-09-15 14:50:43 -0500

    make copper ore deposits a bit bigger

--- src/Swarm/Game/WorldGen.hs
+++ src/Swarm/Game/WorldGen.hs
@@ -81,8 +81,8 @@ testWorld2 ix@(r,c)
       | h `mod` 120 == 1  = (StoneT, Just "lambda")
       | otherwise = (StoneT, Nothing)
     genBiome Big Hard Artificial
-      | sample ix cl0 > 0.9 = (StoneT, Just "copper ore")
-      | otherwise           = (StoneT, Nothing)
+      | sample ix cl0 > 0.85 = (StoneT, Just "copper ore")
+      | otherwise            = (StoneT, Nothing)
 
     sample (i,j) noise = noiseValue noise (fromIntegral i / 2, fromIntegral j / 2, 0)
 
