eaf62f24ebe5262dc82a03e6f089d2fa812d455a
Luis Morillo <luis_cnnvd@hotmail.com>
2021-09-26 12:23:13 +0200

    When 'c' is press the center should go to base, hence the the focusedRobotName should change to base.

--- src/Swarm/TUI/Controller.hs
+++ src/Swarm/TUI/Controller.hs
@@ -395,6 +395,7 @@ handleWorldEvent s (VtyEvent (V.EvKey k []))
 handleWorldEvent s (VtyEvent (V.EvKey (V.KChar 'c') [])) = do
   invalidateCacheEntry WorldCache
   continue $ s & gameState . viewCenterRule .~ VCRobot "base"
+               & gameState . focusedRobotName .~ "base"
                & gameState %~ recalcViewCenter
 
 -- pausing and stepping
