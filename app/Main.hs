d617148fc9356835ed859371bd48df9405f24d07
Tristan Cacqueray <tdecacqu@redhat.com>
2021-09-29 01:11:00 +0000

    Add basic `--check filename` command line argument
    This change adds a new readTerm' helper to enable reporting gcc style
    error.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,31 @@
 module Main where
 
+import           Data.Text                      ( unpack )
+import qualified Data.Text.IO                  as Text
 import           Swarm.App
+import           Swarm.Language.Parse
+import           Swarm.Language.Pipeline
+import           System.Environment             ( getArgs )
+import           System.Exit
+
+check :: FilePath -> IO ()
+check fp = do
+  content <- Text.readFile fp
+  let err = case readTerm' content of
+        Right term -> case processParsedTerm' mempty mempty term of
+          Right _ -> Nothing
+          Left  e -> Just $ " " <> unpack e
+        Left e -> Just $ showShortError e
+  case err of
+    Nothing -> exitSuccess
+    Just e  -> do
+      putStrLn $ fp <> ":" <> e
+      exitFailure
 
 main :: IO ()
-main = appMain
+main = do
+  args <- getArgs
+  case args of
+    []              -> appMain
+    ["--check", fp] -> check fp
+    _               -> putStrLn "usage: swarm [--check fp]"
--- src/Swarm/Language/Parse.hs
+++ src/Swarm/Language/Parse.hs
@@ -27,8 +27,7 @@ module Swarm.Language.Parse
 
     -- * Utility functions
 
-  , runParser, runParserTH, readTerm
-
+  , runParser, runParserTH, readTerm, readTerm', showShortError
   ) where
 
 import           Control.Monad.Reader
@@ -37,6 +36,7 @@ import           Data.Char
 import           Data.Maybe                     (fromMaybe)
 import           Data.Text                      (Text)
 import           Data.Void
+import qualified Data.List.NonEmpty             (head)
 import           Witch
 
 import           Control.Monad.Combinators.Expr
@@ -56,6 +56,8 @@ data Antiquoting = AllowAntiquoting | DisallowAntiquoting
   deriving (Eq, Ord, Show)
 
 type Parser = ReaderT Antiquoting (Parsec Void Text)
+
+type ParserError = ParseErrorBundle Text Void
 
 --------------------------------------------------
 -- Lexer
@@ -346,3 +348,16 @@ fully p = sc *> p <* eof
 --   a pretty-printed parse error message.
 readTerm :: Text -> Either Text Term
 readTerm = runParser (fully parseTerm)
+
+-- | A lower-level readTerm which returns the megaparsec bundle error
+--   for precise error reporting.
+readTerm' :: Text -> Either ParserError Term
+readTerm' = parse (runReaderT (fully parseTerm) DisallowAntiquoting) ""
+
+-- | A utility for converting a ParserError into a one line message:
+--   <line-nr>: <error-msg>
+showShortError :: ParserError -> String
+showShortError (ParseErrorBundle e ps) = showLine <> ": " <> showError
+  where
+    showError = parseErrorTextPretty (Data.List.NonEmpty.head e)
+    showLine = show $ unPos $ sourceLine $ pstateSourcePos ps
