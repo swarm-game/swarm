10e43f0b7bc831b32818d4f5393944372dd99e97
Tristan Cacqueray <tdecacqu@redhat.com>
2021-09-30 02:11:18 +0000

    Integrate optparse-applicative

--- app/Main.hs
+++ app/Main.hs
@@ -5,11 +5,28 @@ import           Data.Text                      ( Text
                                                 , pack
                                                 )
 import qualified Data.Text.IO                  as Text
+import           Options.Applicative
 import           Swarm.App                      ( appMain )
 import           Swarm.Language.LSP             ( lspMain )
 import           Swarm.Language.Pipeline        ( processTerm )
-import           System.Environment             ( getArgs )
 import           System.Exit
+
+data CLI
+  = Run
+  | Format FilePath
+  | LSP
+
+cliParser :: Parser CLI
+cliParser =
+  subparser
+      (  command "format" (info format (progDesc "Format a file"))
+      <> command "lsp"    (info (pure LSP) (progDesc "Start the LSP"))
+      )
+    <|> pure Run
+  where format = Format <$> strArgument (metavar "FILE")
+
+cliInfo :: ParserInfo CLI
+cliInfo = info (cliParser <**> helper) (fullDesc <> header "Swarm game")
 
 -- | Utility function to validate and format swarm-lang code
 formatFile :: FilePath -> Text -> IO ()
@@ -24,9 +41,8 @@ formatFile fp content = do
 
 main :: IO ()
 main = do
-  args <- getArgs
-  case args of
-    []               -> appMain
-    ["--lsp"]        -> lspMain
-    ["--format", fp] -> formatFile fp =<< Text.readFile fp
-    _                -> putStrLn "usage: swarm [--format fp]"
+  cli <- execParser cliInfo
+  case cli of
+    Run       -> appMain
+    Format fp -> formatFile fp =<< Text.readFile fp
+    LSP       -> lspMain
