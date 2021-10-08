ad91a23809b09a51f5d09d23106559f38d5be676
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2021-10-08 15:28:36 +0530

    Use type family for creating robot context

---
+++ src/Swarm/Game/Context.hs
@@ -0,0 +1,62 @@
+-----------------------------------------------------------------------------
+-----------------------------------------------------------------------------
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Swarm.Game.Context where
+
+import Control.Applicative
+import Data.Data
+import Data.Functor.Identity
+import Data.Map
+import Data.Set
+import Swarm.Game.Value
+import Swarm.Language.Capability
+import Swarm.Language.Types
+
+-- | A context is a mapping from variable names to things.
+newtype Ctx t = Ctx {unCtx :: Map Var t}
+  deriving (Eq, Show, Functor, Foldable, Traversable, Data)
+
+-- | Different phases of the program
+data Phase
+  = -- | Command parsing
+    ParseCommand
+  | -- | Checking capabilities for a definition
+    CapabilityCheck
+  | -- | Evaluating a Term
+    EvaluateTerm
+  deriving (Eq, Ord, Show)
+
+-- A context is a mapping from variable name to information
+-- about the variable. There can be multiple "things" that
+-- independently store information about the variable
+-- they are bundled together in the record
+newtype Context (p :: Phase) = Context (Map Var (VarCtx p))
+data VarCtx p = VarCtx
+  { -- Set of capaibilities required to compute a variable
+    varCaps :: CapsForPhase p (Set Capability)
+  , -- Type of the value the variable stores
+    varType :: TypeForPhase p Polytype
+  , -- Value of the variable
+    varVal :: ValForPhase p Value
+  }
+
+type family CapsForPhase (p :: Phase) :: * -> * where
+  CapsForPhase 'ParseCommand = Maybe
+  CapsForPhase 'CapabilityCheck = Maybe
+  CapsForPhase 'EvaluateTerm = Const ()
+
+type family TypeForPhase (p :: Phase) :: * -> * where
+  TypeForPhase 'ParseCommand = Identity
+  TypeForPhase 'CapabilityCheck = Maybe
+  TypeForPhase 'EvaluateTerm = Const ()
+
+type family ValForPhase (p :: Phase) :: * -> * where
+  ValForPhase 'ParseCommand = Identity
+  ValForPhase 'CapabilityCheck = Maybe
+  ValForPhase 'EvaluateTerm = Const ()
