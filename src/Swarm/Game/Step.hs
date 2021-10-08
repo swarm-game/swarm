999f89f5f3c89392a40c0fcff0acaa0ae350e173
Paul Brauner <polux@google.com>
2021-10-08 14:01:35 +0200

    always keep base in the active robots set

--- src/Swarm/Game/Step.hs
+++ src/Swarm/Game/Step.hs
@@ -84,7 +84,7 @@ gameTick = do
               Just wakeUpTime ->
                 sleepUntil rn wakeUpTime
               Nothing ->
-                unless (isActive curRobot') do
+                unless (isActive curRobot' || rn == "base") do
                   sleepForever rn
 
   -- See if the base is finished with a computation, and if so, record
