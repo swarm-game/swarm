b5eefab68a9398b6e07d57c8634956a1e69afa3d
Brent Yorgey <byorgey@gmail.com>
2021-10-10 17:37:25 -0500

    `salvage` command now uses a separate `plasma cutter` device

--- src/Swarm/Game/Step.hs
+++ src/Swarm/Game/Step.hs
@@ -942,7 +942,8 @@ execConst c vs k = do
         let -- Standard devices that are always installed.
             -- XXX in the future, make a way to build these and just start the base
             -- out with a large supply of each?
-            stdDeviceList = ["treads", "grabber", "solar panel", "detonator", "scanner"]
+            stdDeviceList =
+              ["treads", "grabber", "solar panel", "detonator", "scanner", "plasma cutter"]
             stdDevices = S.fromList $ mapMaybe (`lookupEntityName` em) stdDeviceList
 
             -- Find out what capabilities are required by the program that will
