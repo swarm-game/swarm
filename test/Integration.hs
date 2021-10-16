5ea4da7d8edd7c3d08c05ac034a1a2d47aec4105
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2021-10-16 15:08:13 +0530

    Add tests for examples

--- test/Integration.hs
+++ test/Integration.hs
@@ -4,31 +4,34 @@
 -- | Swarm unit tests
 module Main where
 
-import Data.Either
 import Control.Monad
 import Data.Text (Text)
-import qualified Data.Text as T
+import Data.Functor
+import Data.Either
+import Swarm.Language.Pipeline (processTerm)
+import System.Directory (doesFileExist, listDirectory)
+import System.FilePath.Posix
 import Test.Tasty
 import Test.Tasty.HUnit
 import Witch
 
-import Swarm.Language.Pretty
-import Swarm.Language.Syntax hiding (mkOp)
-import Swarm.Language.Pipeline (processTerm)
-import System.Directory (doesFileExist, listDirectory)
-
 main :: IO ()
-main = defaultMain (withResource acquire exampleTests)
+main = do
+  paths <- acquire
+  defaultMain . exampleTests $ paths
 
-exampleTests :: IO [(FilePath, String)] -> TestTree
-exampleTests = testGroup "Examples test" (mapM exampleTest)
+exampleTests :: [(FilePath, String)] -> TestTree
+exampleTests inputs = testGroup "Test example" (map exampleTest inputs)
 
-exampleTest (path, fileContents) =
-  testCase ("processTerm for contents of " ++ show path)
-    $ assertBool "should return Right value" (isRight . processTerm $ into @Text fileContents)
+exampleTest :: (FilePath, String) -> TestTree
+exampleTest (path, fileContent) = do
+  testCase ("processTerm for contents of " ++ show path) $
+    assertBool "should return Right value" (isRight . processTerm $ into @Text fileContent)
 
 acquire :: IO [(FilePath, String)]
 acquire = do
-  paths <- listDirectory "example"
+  paths <- listDirectory exampleDirectory <&> map (exampleDirectory </>)
   filePaths <- filterM doesFileExist paths
-  mapM (\path -> (path, readFile path)) filePaths
+  mapM (\path -> (,) path <$> readFile path) filePaths
+  where
+    exampleDirectory = "example"
