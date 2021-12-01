cac49f23d727a28e34c8b70ef639b9f7eeef3ac8
Brent Yorgey <byorgey@gmail.com>
2021-12-01 10:45:19 -0600

    generalize `scan` to return `() + string`
    Players can ignore the return value until they have the capability to
    deal with sum types.  `scan` still has the effect of adding
    information to one's inventory.
    
    Closes #267.

--- src/Swarm/Language/Typecheck.hs
+++ src/Swarm/Language/Typecheck.hs
@@ -445,7 +445,7 @@ inferConst c = toU $ case c of
   Create -> [tyQ| string -> cmd () |]
   Whereami -> [tyQ| cmd (int * int) |]
   Blocked -> [tyQ| cmd bool |]
-  Scan -> [tyQ| dir -> cmd () |]
+  Scan -> [tyQ| dir -> cmd (() + string) |]
   Upload -> [tyQ| string -> cmd () |]
   Ishere -> [tyQ| string -> cmd bool |]
   Whoami -> [tyQ| cmd string |]
