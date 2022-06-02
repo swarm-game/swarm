8238318302272bf169885fd4eeb55050911ffef6
Brent Yorgey <byorgey@gmail.com>
2022-06-02 16:49:54 -0500

    reorganize scenarios

--- test/Unit.hs
+++ test/Unit.hs
@@ -35,7 +35,7 @@ import Swarm.TUI.Model
 
 main :: IO ()
 main = do
-  mg <- runExceptT (initGameState (Just 0) (Just "classic") Nothing)
+  mg <- runExceptT (initGameState (Just 0) (Just "00-classic") Nothing)
   case mg of
     Left err -> assertFailure (from err)
     Right g -> defaultMain (tests g)
