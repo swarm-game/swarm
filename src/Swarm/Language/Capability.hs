7b4ffccb9d39a6acb09a3cc950ec664740302414
Brent Yorgey <byorgey@gmail.com>
2022-06-06 07:17:46 -0500

    add sand, glass, and calculator (#362)
    - Sand is now found near water.  Sand regenerates immediately (like water).
    - Sand can be used in a furnace to make glass.
    - Glass + copper wires + 3D printer can be used to make solar panels.
        - Note that with #361 I intend to start the base with a small number of 3D printers, so other robots could carry this out.  But also, the base will start with a bunch of solar panels too.
    - Solar panel + counter can be used to make a calculator.

--- src/Swarm/Language/Capability.hs
+++ src/Swarm/Language/Capability.hs
@@ -266,6 +266,12 @@ constCaps =
     Salvage -> [CSalvage]
     Reprogram -> [CReprogram]
     Drill -> [CDrill]
+    Neg -> [CArith]
+    Add -> [CArith]
+    Sub -> [CArith]
+    Mul -> [CArith]
+    Div -> [CArith]
+    Exp -> [CArith]
     -- Some God-like sensing abilities.
     As -> [CGod]
     RobotNamed -> [CGod]
@@ -278,7 +284,6 @@ constCaps =
     Appear -> [CAppear] -- paint?
     Whereami -> [CSenseloc] -- GPS?
     Random -> [CRandom] -- randomness device (with bitcoins)?
-    Neg -> [CArith] -- ALU? pocket calculator?
     Whoami -> [CWhoami] -- mirror, needs a recipe
 
     -- comparator?
@@ -290,11 +295,6 @@ constCaps =
     Geq -> [CCompare]
     And -> []
     Or -> []
-    Add -> [CArith]
-    Sub -> [CArith]
-    Mul -> [CArith]
-    Div -> [CArith]
-    Exp -> [CArith]
     -- Some more constants which *ought* to have their own capability but
     -- currently don't.
     Say -> []
