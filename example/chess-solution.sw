80540c887df06e82a5ba7b0a479580b7bf8a46be
Ondřej Šebek <xsebek@fi.muni.cz>
2022-06-12 20:10:21 +0200

    WIP: scenario test

---
+++ example/chess-solution.sw
@@ -0,0 +1,1 @@
+move; move; move; turn right; move; turn left; move;
--- test/integration/Main.hs
+++ test/integration/Main.hs
@@ -6,6 +6,7 @@ module Main where
 
 import Control.Monad
 import Data.Functor
+import qualified Data.IntMap as IM
 import Data.Text (Text)
 import Data.Yaml (ParseException, prettyPrintParseException)
 import System.Directory (doesFileExist, listDirectory)
@@ -13,11 +14,20 @@ import System.FilePath.Posix
 import Test.Tasty
 import Test.Tasty.HUnit
 import Witch
-
+import Control.Monad.State
 import Swarm.Game.Entity
 import Swarm.Game.Scenario
 import Swarm.Language.Pipeline (processTerm)
+import Swarm.Language.Context as Ctx
 import Swarm.Util.Yaml (decodeFileEitherE)
+import Swarm.Game.State
+import Control.Monad.Trans.Except (runExceptT)
+import Swarm.Game.CESK
+import Swarm.Game.Value
+import Swarm.Game.Robot
+import Swarm.Game.Step
+import Swarm.Game.Exception
+import Control.Lens hiding (from)
 
 main :: IO ()
 main = do
@@ -33,6 +43,7 @@ main = do
           "Tests"
           [ exampleTests examplePaths
           , scenarioTests em scenarioPaths
+          , testScenarioSolution em
           ]
 
 exampleTests :: [(FilePath, String)] -> TestTree
@@ -50,11 +61,14 @@ scenarioTests em inputs = testGroup "Test scenarios" (map (scenarioTest em) inpu
 
 scenarioTest :: EntityMap -> (FilePath, String) -> TestTree
 scenarioTest em (path, _) =
-  testCase ("parse scenario " ++ show path) $ do
-    res <- decodeFileEitherE em path :: IO (Either ParseException Scenario)
-    case res of
-      Left err -> assertFailure (prettyPrintParseException err)
-      Right _ -> return ()
+  testCase ("parse scenario " ++ show path) (void $ getScenario em path)
+
+getScenario :: EntityMap -> FilePath -> IO Scenario
+getScenario em p = do
+  res <- decodeFileEitherE em p :: IO (Either ParseException Scenario)
+  case res of
+    Left err -> assertFailure (prettyPrintParseException err)
+    Right s -> return s
 
 acquire :: FilePath -> String -> IO [(FilePath, String)]
 acquire dir ext = do
@@ -63,3 +77,38 @@ acquire dir ext = do
   mapM (\path -> (,) path <$> readFile path) filePaths
  where
   hasExt path = takeExtension path == ("." ++ ext)
+
+testScenarioSolution :: EntityMap -> TestTree
+testScenarioSolution em =
+  testGroup
+    "Test scenario solutions"
+    [ testSolution "chess" "data/scenarios/03Challenges/01-chess_horse.yaml" "example/chess-solution.sw"
+    ]
+ where
+  testSolution :: TestName -> FilePath -> FilePath -> TestTree
+  testSolution n p r = testCase n $ do
+    Right gs <- runExceptT $ initGameState Nothing (Just p) (Just r)
+    v <- evalScenario gs
+    case v of
+      Left err -> assertFailure ("Evaluation failed: " ++ from @Text @String err)
+      Right (VBool True) -> return ()
+      Right v -> assertFailure ("Not what I hoped for: " <> show v)
+
+  evalScenario :: GameState -> IO (Either Text Value)
+  evalScenario g = evaluateCESK (initMachine w Ctx.empty emptyStore) g
+   where
+    w = case g ^. winCondition of
+      WinCondition win -> win
+      _ -> error "no win condition"
+
+  evaluateCESK :: CESK -> GameState -> IO (Either Text Value)
+  evaluateCESK cesk g = flip evalStateT g . flip evalStateT r . runCESK $ cesk
+   where
+    r :: Robot
+    r = (g ^. robotMap) IM.! 0
+
+  runCESK :: CESK -> StateT Robot (StateT GameState IO) (Either Text Value)
+  runCESK (Up exn _ []) = return (Left (formatExn em exn))
+  runCESK cesk = case finalValue cesk of
+    Just (v, _) -> return (Right v)
+    Nothing -> stepCESK cesk >>= runCESK
