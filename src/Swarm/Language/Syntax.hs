65427e2e3f610497f4b62727fc4b649e45b854fc
Restyled.io <commits@restyled.io>
2022-07-05 02:39:12 +0000

    Restyled by fourmolu

--- src/Swarm/Language/Syntax.hs
+++ src/Swarm/Language/Syntax.hs
@@ -113,9 +113,9 @@ allDirs = [minBound .. maxBound]
 -- | Information about all directions
 dirInfo :: Direction -> DirInfo
 dirInfo d = case d of
-  DLeft -> relative (\(V2 x y) -> V2 (-y) x)
-  DRight -> relative (\(V2 x y) -> V2 y (-x))
-  DBack -> relative (\(V2 x y) -> V2 (-x) (-y))
+  DLeft -> relative (\(V2 x y) -> V2 (- y) x)
+  DRight -> relative (\(V2 x y) -> V2 y (- x))
+  DBack -> relative (\(V2 x y) -> V2 (- x) (- y))
   DDown -> relative (const down)
   DForward -> relative id
   DNorth -> cardinal north
