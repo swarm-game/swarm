d9686f32e08a4e60dc8cab0fe6b27162a7e00dc7
Ondřej Šebek <xsebek@fi.muni.cz>
2022-09-03 22:18:06 +0200

    WIP: remove language template haskell pragma

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
 
 module Main where
 
--- src/Swarm/App.hs
+++ src/Swarm/App.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
 
 -- |
 -- Module      :  Swarm.App
