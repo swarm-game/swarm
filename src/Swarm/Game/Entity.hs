0e09d47c1f67bd9b5f8c0997944421a98975a08a
Karl Ostmo <kostmo@gmail.com>
2022-11-05 14:05:53 -0700

    Drills by capability (#830)
    This PR consists of two commits:
    1. Refactor list of capabilities to sets (continuation of #794)
    2. Instead of hard-coding drill names, select equipped entities by their `drill` capability
    
    This allows scenario authors to define their own drills.

--- src/Swarm/Game/Entity.hs
+++ src/Swarm/Game/Entity.hs
@@ -69,6 +69,7 @@ module Swarm.Game.Entity (
   isSubsetOf,
   isEmpty,
   inventoryCapabilities,
+  extantElemsWithCapability,
 
   -- ** Modification
   insert,
@@ -81,7 +82,7 @@ module Swarm.Game.Entity (
 ) where
 
 import Control.Arrow ((&&&))
-import Control.Lens (Getter, Lens', lens, to, view, (^.), _2)
+import Control.Lens (Getter, Lens', lens, to, view, (^.))
 import Control.Monad.IO.Class
 import Data.Bifunctor (bimap, first)
 import Data.Char (toLower)
@@ -97,8 +98,7 @@ import Data.Map (Map)
 import Data.Map qualified as M
 import Data.Maybe (fromMaybe, isJust, listToMaybe)
 import Data.Set (Set)
-import Data.Set qualified as Set (fromList)
-import Data.Set.Lens (setOf)
+import Data.Set qualified as Set (fromList, member, toList, unions)
 import Data.Text (Text)
 import Data.Text qualified as T
 import Data.Yaml
@@ -222,7 +222,7 @@ data Entity = Entity
   , -- | Properties of the entity.
     _entityProperties :: Set EntityProperty
   , -- | Capabilities provided by this entity.
-    _entityCapabilities :: [Capability]
+    _entityCapabilities :: Set Capability
   , -- | Inventory of other entities held by this entity.
     _entityInventory :: Inventory
   }
@@ -275,7 +275,7 @@ mkEntity ::
   [Capability] ->
   Entity
 mkEntity disp nm descr props caps =
-  rehashEntity $ Entity 0 disp nm Nothing descr Nothing Nothing Nothing (Set.fromList props) caps empty
+  rehashEntity $ Entity 0 disp nm Nothing descr Nothing Nothing Nothing (Set.fromList props) (Set.fromList caps) empty
 
 ------------------------------------------------------------
 -- Entity map
@@ -313,7 +313,7 @@ buildEntityMap :: [Entity] -> EntityMap
 buildEntityMap es =
   EntityMap
     { entitiesByName = M.fromList . map (view entityName &&& id) $ es
-    , entitiesByCap = M.fromListWith (<>) . concatMap (\e -> map (,[e]) (e ^. entityCapabilities)) $ es
+    , entitiesByCap = M.fromListWith (<>) . concatMap (\e -> map (,[e]) (Set.toList $ e ^. entityCapabilities)) $ es
     }
 
 ------------------------------------------------------------
@@ -332,7 +332,7 @@ instance FromJSON Entity where
               <*> v .:? "growth"
               <*> v .:? "yields"
               <*> v .:? "properties" .!= mempty
-              <*> v .:? "capabilities" .!= []
+              <*> v .:? "capabilities" .!= mempty
               <*> pure empty
           )
 
@@ -440,7 +440,7 @@ hasProperty :: Entity -> EntityProperty -> Bool
 hasProperty e p = p `elem` (e ^. entityProperties)
 
 -- | The capabilities this entity provides when installed.
-entityCapabilities :: Lens' Entity [Capability]
+entityCapabilities :: Lens' Entity (Set Capability)
 entityCapabilities = hashedLens _entityCapabilities (\e x -> e {_entityCapabilities = x})
 
 -- | The inventory of other entities carried by this entity.
@@ -567,7 +567,17 @@ isEmpty = all ((== 0) . fst) . elems
 
 -- | Compute the set of capabilities provided by the devices in an inventory.
 inventoryCapabilities :: Inventory -> Set Capability
-inventoryCapabilities = setOf (to elems . traverse . _2 . entityCapabilities . traverse)
+inventoryCapabilities = Set.unions . map (^. entityCapabilities) . nonzeroEntities
+
+-- | List elements that have at least one copy in the inventory.
+nonzeroEntities :: Inventory -> [Entity]
+nonzeroEntities = map snd . filter ((> 0) . fst) . elems
+
+-- | List elements that possess a given Capability and
+-- exist with nonzero count in the inventory.
+extantElemsWithCapability :: Capability -> Inventory -> [Entity]
+extantElemsWithCapability cap =
+  filter (Set.member cap . (^. entityCapabilities)) . nonzeroEntities
 
 -- | Delete a single copy of a certain entity from an inventory.
 delete :: Entity -> Inventory -> Inventory
