8ddc6887f2ed6c709e9c0092d6ca8a234e4279e7
Brent Yorgey <byorgey@gmail.com>
2022-11-06 13:52:26 -0600

    do not warn about unlisted scenario subdirs starting with lowercase
    Such subdirectories can be used to e.g. organize `.sw` files.  If they
    are not listed in `00-ORDER.txt` and start with a lowercase letter,
    there will no longer be a warning about them.

--- src/Swarm/Game/ScenarioInfo.hs
+++ src/Swarm/Game/ScenarioInfo.hs
@@ -57,7 +57,7 @@ import Data.Aeson (
   genericToEncoding,
   genericToJSON,
  )
-import Data.Char (isSpace, toLower)
+import Data.Char (isLower, isSpace, toLower)
 import Data.Function (on)
 import Data.List (intercalate, stripPrefix, (\\))
 import Data.Map (Map)
@@ -260,7 +260,7 @@ loadScenarioDir em dir = do
             <> ", using alphabetical order"
       return Nothing
     True -> Just . filter (not . null) . lines <$> sendIO (readFile orderFile)
-  fs <- sendIO $ keepYamlOrDirectory <$> listDirectory dir
+  fs <- sendIO $ keepYamlOrNonLowerDirectory <$> listDirectory dir
 
   case morder of
     Just order -> do
@@ -287,7 +287,11 @@ loadScenarioDir em dir = do
   let morder' = filter (`elem` fs) <$> morder
   SC morder' . M.fromList <$> mapM (\item -> (item,) <$> loadScenarioItem em (dir </> item)) fs
  where
-  keepYamlOrDirectory = filter (\f -> takeExtensions f `elem` ["", ".yaml"])
+  -- Keep only files which are .yaml files or directories that start
+  -- with something other than a lowercase letter.
+  keepYamlOrNonLowerDirectory =
+    filter
+      (\f -> takeExtensions f == ".yaml" || (takeExtensions f == "" && not (isLower (head f))))
 
 -- | How to transform scenario path to save path.
 scenarioPathToSavePath :: FilePath -> FilePath -> FilePath
