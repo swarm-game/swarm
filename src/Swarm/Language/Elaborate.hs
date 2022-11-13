0ee3c76054a590cb795ffa066c923a334da96d7e
Brent Yorgey <byorgey@gmail.com>
2022-11-13 17:57:41 -0600

    WIP: work on converting `Elaborate`

--- src/Swarm/Language/Elaborate.hs
+++ src/Swarm/Language/Elaborate.hs
@@ -8,7 +8,7 @@
 -- Term elaboration which happens after type checking.
 module Swarm.Language.Elaborate where
 
-import Control.Lens (transform, (%~))
+import Control.Lens (transform, (%~), (^.))
 import Swarm.Language.Syntax
 import Swarm.Language.Types
 
@@ -24,10 +24,11 @@ elaborate =
   -- Wrap all *free* variables in 'Force'.  Free variables must be
   -- referring to a previous definition, which are all wrapped in
   -- 'TDelay'.
-  (fvT %~ TApp (TConst Force))
+  (fvT %~ \s -> Syntax' (s ^. sLoc) (SApp (TConst Force) s) (s ^. sType))
     -- Now do additional rewriting on all subterms.
     . transform rewrite
  where
+  rewrite :: Syntax' Polytype -> Syntax' Polytype
   -- For recursive let bindings, rewrite any occurrences of x to
   -- (force x).  When interpreting t1, we will put a binding (x |->
   -- delay t1) in the context.
@@ -44,5 +45,5 @@ elaborate =
   -- Leave any other subterms alone.
   rewrite t = t
 
-wrapForce :: Var -> Term -> Term
+wrapForce :: Var -> Syntax' Polytype -> Syntax' Polytype
 wrapForce x = mapFree1 x (TApp (TConst Force))
