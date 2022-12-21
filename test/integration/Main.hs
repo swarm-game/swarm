9d615b78f57336f3bde5e50fa70e257441ec2aa8
Brent Yorgey <byorgey@gmail.com>
2022-12-20 21:05:21 -0600

    test for `meet` and `meetAll`

--- test/integration/Main.hs
+++ test/integration/Main.hs
@@ -214,6 +214,7 @@ testScenarioSolution _ci _em =
         , testSolution Default "Testing/699-movement-fail/699-move-liquid"
         , testSolution Default "Testing/699-movement-fail/699-teleport-blocked"
         , testSolution Default "Testing/710-multi-robot"
+        , testSolution Default "Testing/920-meet"
         ]
     ]
  where
