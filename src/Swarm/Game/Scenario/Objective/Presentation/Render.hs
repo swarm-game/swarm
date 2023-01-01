32be92d30a4ae5398c3d2e29e00b4f429bf42958
Karl Ostmo <kostmo@gmail.com>
2022-12-31 17:07:50 -0800

    WIP goals dialog

--- src/Swarm/Game/Scenario/Objective/Presentation/Render.hs
+++ src/Swarm/Game/Scenario/Objective/Presentation/Render.hs
@@ -2,32 +2,60 @@
 
 module Swarm.Game.Scenario.Objective.Presentation.Render where
 
+import Data.Vector qualified as V
 import Brick hiding (Direction, Location)
 import Control.Lens hiding (Const, from)
-import Data.List (intercalate, intersperse)
-import Data.List.NonEmpty (NonEmpty)
+import Data.Maybe (listToMaybe)
+import Brick.Widgets.List qualified as BL
 import Data.List.NonEmpty qualified as NE
 import Data.Map.Strict qualified as M
 import Swarm.Game.Scenario.Objective
 import Swarm.Game.Scenario.Objective.Presentation.Model
 import Swarm.TUI.Model.Name
 import Swarm.TUI.View.Util
+import Swarm.TUI.Attr (greenAttr, boldAttr)
+
+data GoalEntry
+  = Header GoalStatus
+  | Goal GoalStatus Objective
+
+makeListWidget :: GoalDisplay -> BL.List Name GoalEntry
+makeListWidget (GoalDisplay _announcements categorizedObjs) =
+  BL.listMoveTo 1 $ BL.list ObjectivesList (V.fromList objList) 1
+  where
+    objList = concatMap f $ M.toList categorizedObjs
+    f (h, xs) = Header h : map (Goal h) (NE.toList xs)
+
 
 renderGoalsDisplay :: GoalDisplay -> Widget Name
-renderGoalsDisplay (GoalDisplay _announcements categorizedObjs) =
-  vBox $
-    intercalate [txt "========="] $
-      map renderGoalsCategory $
-        M.toList categorizedObjs
- where
-  renderGoalsCategory :: (GoalStatus, NonEmpty Objective) -> [Widget Name]
-  renderGoalsCategory (category, objs) =
-    header : intersperse (txt "--------") (objectiveTexts objs)
-   where
-    header = str $ unwords [">>", show category, "<<"]
+renderGoalsDisplay gd =
+  hBox
+    [ padAll 2 $
+          BL.renderList (const drawAchievementListItem) True listWidget
+    , maybe emptyWidget (singleGoalDetails . snd) $
+          BL.listSelectedElement listWidget
+    ]
+  where
+    listWidget = makeListWidget gd
 
-  objectiveTexts :: NonEmpty Objective -> [Widget Name]
-  objectiveTexts = map renderSingleGoal . NE.toList
+getCompletionIcon :: Bool -> Widget Name
+getCompletionIcon = \case
+  False -> txt " ○  "
+  True -> withAttr greenAttr $ txt " ●  "
 
-renderSingleGoal :: Objective -> Widget Name
-renderSingleGoal g = displayParagraphs $ g ^. objectiveGoal
+drawAchievementListItem ::
+  GoalEntry ->
+  Widget Name
+drawAchievementListItem = \case
+  Header gs -> withAttr boldAttr $ str $ show gs
+  Goal _gs obj -> getCompletionIcon True <+> titleWidget
+    where
+      titleWidget = case listToMaybe $ obj ^. objectiveGoal of
+        Nothing -> txt "?"
+        Just x -> txt x
+
+
+singleGoalDetails :: GoalEntry -> Widget Name
+singleGoalDetails = \case
+  Header gs -> emptyWidget
+  Goal gs obj -> displayParagraphs $ obj ^. objectiveGoal
