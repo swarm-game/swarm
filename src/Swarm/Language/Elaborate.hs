ca2df55f9a4c689e2492b8f0299d9f465b699968
Brent Yorgey <byorgey@gmail.com>
2023-01-09 21:54:40 -0600

    WIP: fix (??) elaboration

--- src/Swarm/Language/Elaborate.hs
+++ src/Swarm/Language/Elaborate.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 -- |
 -- Module      :  Swarm.Language.Elaborate
 -- Copyright   :  Brent Yorgey
@@ -24,31 +26,35 @@ elaborate =
   -- Wrap all *free* variables in 'Force'.  Free variables must be
   -- referring to a previous definition, which are all wrapped in
   -- 'TDelay'.
-  (fvT %~ \s -> Syntax' (s ^. sLoc) (SApp (TConst Force) s) (s ^. sType))
+  (fvSS %~ \s -> Syntax' (s ^. sLoc) (SApp sForce s) (s ^. sType))
     -- Now do additional rewriting on all subterms.
     . transform rewrite
  where
   rewrite :: Syntax' Polytype -> Syntax' Polytype
-  rewrite s@(Syntax' l t ty) = Syntax' l (rewriteTerm t) ty
+  rewrite (Syntax' l t ty) = Syntax' l (rewriteTerm t) ty
 
   rewriteTerm :: Term' Polytype -> Term' Polytype
-  -- For recursive let bindings, rewrite any occurrences of x to
-  -- (force x).  When interpreting t1, we will put a binding (x |->
-  -- delay t1) in the context.
-  rewriteTerm (TLet True x ty t1 t2) = TLet True x ty (wrapForce x t1) (wrapForce x t2)
-  -- Rewrite any recursive occurrences of x inside t1 to (force x).
-  -- When a TDef is encountered at runtime its body will immediately
-  -- be wrapped in a VDelay. However, to make this work we also need
-  -- to wrap all free variables in any term with 'force' --- since
-  -- any such variables must in fact refer to things previously
-  -- bound by 'def'.
-  rewriteTerm (TDef True x ty t1) = TDef True x ty (mapFree1 x (TApp (TConst Force)) t1)
-  -- Rewrite @f $ x@ to @f x@.
-  rewriteTerm (TApp (TApp (TConst AppF) r) l) = TApp r l
-  -- Leave any other subterms alone.
-  rewriteTerm t = t
+  rewriteTerm = \case
+    -- For recursive let bindings, rewrite any occurrences of x to
+    -- (force x).  When interpreting t1, we will put a binding (x |->
+    -- delay t1) in the context.
+    SLet True x ty t1 t2 -> SLet True x ty (wrapForce x t1) (wrapForce x t2)
+    -- Rewrite any recursive occurrences of x inside t1 to (force x).
+    -- When a TDef is encountered at runtime its body will immediately
+    -- be wrapped in a VDelay. However, to make this work we also need
+    -- to wrap all free variables in any term with 'force' --- since
+    -- any such variables must in fact refer to things previously
+    -- bound by 'def'.
+    SDef True x ty t1 -> SDef True x ty (wrapForce x t1)
+    -- Rewrite @f $ x@ to @f x@.
+    SApp (Syntax' _ (SApp (Syntax' _ (TConst AppF) _) l) _) r -> SApp l r
+    -- Leave any other subterms alone.
+    t -> t
 
 wrapForce :: Var -> Syntax' Polytype -> Syntax' Polytype
-wrapForce x = mapFree1 x (\s@(Syntax' l _ ty) -> Syntax' l (SApp (Syntax' mempty (TConst Force) _) s) ty) -- XXX
+wrapForce x = mapFreeS x (\s@(Syntax' l _ ty) -> Syntax' l (SApp sForce s) ty)
 
--- (TApp (TConst Force))
+-- Note, TyUnit is not the right type, but I don't want to bother
+
+sForce :: Syntax' Polytype
+sForce = Syntax' NoLoc (TConst Force) (Forall ["a"] (TyDelay (TyVar "a") :->: TyVar "a"))
