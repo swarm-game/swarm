cf2612b4098e451845e5713c79895b0fc877daef
Karl Ostmo <kostmo@gmail.com>
2023-01-15 23:13:46 -0800

    elaborate comment

--- scripts/play.sh
+++ scripts/play.sh
@@ -3,5 +3,8 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR/..
 
+# This compiles without optimizations and then runs the resulting executable.
+# It's been observed in certain versions of GHC that compiling with optimizations
+# results in the swarm UI freezing for a potentially long time upon starting a scenario.
 # See https://github.com/swarm-game/swarm/issues/1000#issuecomment-1378632269
 stack build --fast && stack exec swarm -- $@
