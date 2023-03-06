88bad10da867247b8bf6ca089aaa6a334b266843
Brent Yorgey <byorgey@gmail.com>
2023-03-06 15:11:41 -0600

    add random "static" to `view` outside a certain range (#1110)
    The basic principle here is that you cannot communicate with robots that are too far away.  We also want to uphold the principle that it is not possible to instantly communicate information over arbitrary distances.
    
    Concretely, there is now an "inner radius" (16 by default) and an "outer radius" (64 by default).
    * Inside of the inner radius, communication is perfect.
    * Between the inner and outer radius, the `view` becomes increasingly covered with "static".
    * Outside the outer radius, the view is completely static, *and* it is impossible to see a viewed robot's inventory.
    
    In particular there should be absolutely no difference between trying to `view` a robot that is deceased vs a robot that is too far away.  A nice bonus feature is that if you are `view`ing a robot that is suddenly destroyed, e.g. by driving into the water, the entire view instantly turns to static, which looks cool.
    
    The static is based on a hash of the offset from the view center to the cell + the current tick count / 16.  That means that the static pattern (1) doesn't shift when the viewed robot moves around, and (2) randomly changes every second or so.
    
    [See this asciinema cast](https://asciinema.org/a/564988) for a demo of it in action.
    
    Closes #17 .
    
    - [x] update `antenna` description

--- src/Swarm/Game/State.hs
+++ src/Swarm/Game/State.hs
@@ -94,7 +94,10 @@ module Swarm.Game.State (
   recalcViewCenter,
   modifyViewCenter,
   viewingRegion,
+  unfocus,
   focusedRobot,
+  RobotRange (..),
+  focusedRange,
   clearFocusedRobotLogUpdated,
   addRobot,
   addTRobot,
@@ -694,6 +697,13 @@ modifyViewCenter update g =
       VCLocation l -> viewCenterRule .~ VCLocation (update l)
       VCRobot _ -> viewCenterRule .~ VCLocation (update (g ^. viewCenter))
 
+-- | "Unfocus" by modifying the view center rule to look at the
+--   current location instead of a specific robot, and also set the
+--   focused robot ID to an invalid value.  In classic mode this
+--   causes the map view to become nothing but static.
+unfocus :: GameState -> GameState
+unfocus = (\g -> g {_focusedRobotID = -1000}) . modifyViewCenter id
+
 -- | Given a width and height, compute the region, centered on the
 --   'viewCenter', that should currently be in view.
 viewingRegion :: GameState -> (Int32, Int32) -> (W.Coords, W.Coords)
@@ -707,6 +717,64 @@ viewingRegion g (w, h) = (W.Coords (rmin, cmin), W.Coords (rmax, cmax))
 --   'viewCenterRule', if any.
 focusedRobot :: GameState -> Maybe Robot
 focusedRobot g = g ^. robotMap . at (g ^. focusedRobotID)
+
+-- | Type for describing how far away a robot is from the base, which
+--   determines what kind of communication can take place.
+data RobotRange
+  = -- | Close; communication is perfect.
+    Close
+  | -- | Mid-range; communication is possible but lossy.
+    MidRange Double
+  | -- | Far; communication is not possible.
+    Far
+  deriving (Eq, Ord)
+
+-- | Check how far away the focused robot is from the base.  @Nothing@
+--   is returned if there is no focused robot; otherwise, return a
+--   'RobotRange' value as follows.
+--
+--   * If we are in creative or scroll-enabled mode, the focused robot is
+--   always considered 'Close'.
+--   * Otherwise, there is a "minimum radius" and "maximum radius".
+--       - If the robot is within the minimum radius, it is 'Close'.
+--       - If the robot is between the minimum and maximum radii, it
+--         is 'MidRange', with a 'Double' value ranging linearly from
+--         0 to 1 proportional to the distance from the minimum to
+--         maximum radius.  For example, 'MidRange 0.5' would indicate
+--         a robot exactly halfway between the minimum and maximum
+--         radii.
+--       - If the robot is beyond the maximum radius, it is 'Far'.
+--   * By default, the minimum radius is 16, and maximum is 64.
+--   * If the focused robot has an @antenna@ installed, it doubles
+--     both radii.
+--   * If the base has an @antenna@ installed, it also doubles both radii.
+focusedRange :: GameState -> Maybe RobotRange
+focusedRange g = computedRange <$ focusedRobot g
+ where
+  computedRange
+    | g ^. creativeMode || g ^. worldScrollable || r <= minRadius = Close
+    | r > maxRadius = Far
+    | otherwise = MidRange $ (r - minRadius) / (maxRadius - minRadius)
+
+  -- Euclidean distance from the base to the view center.
+  r = case g ^. robotMap . at 0 of
+    Just br -> euclidean (g ^. viewCenter) (br ^. robotLocation)
+    _ -> 1000000000 -- if the base doesn't exist, we have bigger problems
+
+  -- See whether the base or focused robot have antennas installed.
+  baseInv, focInv :: Maybe Inventory
+  baseInv = g ^? robotMap . ix 0 . equippedDevices
+  focInv = view equippedDevices <$> focusedRobot g
+
+  gain :: Maybe Inventory -> (Double -> Double)
+  gain (Just inv)
+    | countByName "antenna" inv > 0 = (* 2)
+  gain _ = id
+
+  -- Range radii.  Default thresholds are 16, 64; each antenna
+  -- boosts the signal by 2x.
+  minRadius, maxRadius :: Double
+  (minRadius, maxRadius) = over both (gain baseInv . gain focInv) (16, 64)
 
 -- | Clear the 'robotLogUpdated' flag of the focused robot.
 clearFocusedRobotLogUpdated :: Has (State GameState) sig m => m ()
--- src/Swarm/TUI/View/CellDisplay.hs
+++ src/Swarm/TUI/View/CellDisplay.hs
@@ -3,19 +3,27 @@
 module Swarm.TUI.View.CellDisplay where
 
 import Brick
-import Control.Lens
+import Control.Lens (to, view, (^.))
+import Data.Bool (bool)
+import Data.ByteString (ByteString)
+import Data.Hash.Murmur
 import Data.List.NonEmpty qualified as NE
 import Data.Map qualified as M
 import Data.Maybe (maybeToList)
 import Data.Semigroup (sconcat)
+import Data.Tagged (unTagged)
+import Data.Word (Word32)
+import Linear.Affine ((.-.))
 import Swarm.Game.Display
-import Swarm.Game.Entity as E
+import Swarm.Game.Entity
 import Swarm.Game.Robot
 import Swarm.Game.State
 import Swarm.Game.Terrain
 import Swarm.Game.World qualified as W
 import Swarm.TUI.Attr
 import Swarm.TUI.Model.Name
+import Witch (from)
+import Witch.Encoding qualified as Encoding
 
 -- | Render a display as a UI widget.
 renderDisplay :: Display -> Widget n
@@ -45,11 +53,24 @@ displayEntityCell g coords = maybeToList (displayForEntity <$> W.lookupEntity co
         HideNoEntity -> True
         HideEntityUnknownTo ro -> ro `robotKnows` e
 
+data HideEntity = HideAllEntities | HideNoEntity | HideEntityUnknownTo Robot
+
+hidingMode :: GameState -> HideEntity
+hidingMode g
+  | g ^. creativeMode = HideNoEntity
+  | otherwise = maybe HideAllEntities HideEntityUnknownTo $ focusedRobot g
+
+-- | Get the 'Display' for a specific location, by combining the
+--   'Display's for the terrain, entity, and robots at the location, and
+--   taking into account "static" based on the distance to the robot
+--   being @view@ed.
+displayLoc :: Bool -> GameState -> W.Coords -> Display
+displayLoc showRobots g coords = addStatic g coords $ displayLocRaw showRobots g coords
+
 -- | Get the 'Display' for a specific location, by combining the
 --   'Display's for the terrain, entity, and robots at the location.
-displayLoc :: Bool -> GameState -> W.Coords -> Display
-displayLoc showRobots g coords =
-  sconcat $ terrain NE.:| entity <> robots
+displayLocRaw :: Bool -> GameState -> W.Coords -> Display
+displayLocRaw showRobots g coords = sconcat $ terrain NE.:| entity <> robots
  where
   terrain = displayTerrainCell g coords
   entity = displayEntityCell g coords
@@ -58,9 +79,47 @@ displayLoc showRobots g coords =
       then displayRobotCell g coords
       else []
 
-data HideEntity = HideAllEntities | HideNoEntity | HideEntityUnknownTo Robot
+-- | Add random "static" based on the distance to the robot being
+--   @view@ed.
+addStatic :: GameState -> W.Coords -> Display -> Display
+addStatic g coords d = maybe d displayStatic (getStatic g coords)
 
-hidingMode :: GameState -> HideEntity
-hidingMode g
-  | g ^. creativeMode = HideNoEntity
-  | otherwise = maybe HideAllEntities HideEntityUnknownTo $ focusedRobot g
+-- | Draw black or white static.
+displayStatic :: Bool -> Display
+displayStatic = bool mempty (terrainMap M.! IceT)
+
+-- | Random "static" based on the distance to the robot being
+--   @view@ed.  A cell can either be static-free (represented by
+--   @Nothing@) or can have one of two types ("black" or "white").
+getStatic :: GameState -> W.Coords -> Maybe Bool
+getStatic g coords
+  | isStatic = Just (even h)
+  | otherwise = Nothing
+ where
+  -- Offset from the location of the view center to the location under
+  -- consideration for display.
+  offset = W.coordsToLoc coords .-. (g ^. viewCenter)
+
+  -- Hash.
+  h =
+    murmur3 1 . unTagged . from @String @(Encoding.UTF_8 ByteString) . show $
+      -- include the current tick count / 16 in the hash, so the pattern of static
+      -- changes once every 16 ticks
+      (offset, (g ^. ticks) `div` 16)
+
+  -- Hashed probability, i.e. convert the hash into a floating-point number between 0 and 1
+  hp :: Double
+  hp = fromIntegral h / fromIntegral (maxBound :: Word32)
+
+  isStatic = case focusedRange g of
+    -- If we're not viewing a robot, display static.  This
+    -- can happen if e.g. the robot we were viewing drowned.
+    Nothing -> True
+    -- Don't display static if the robot is close, or when we're in
+    -- creative mode or the player is allowed to scroll the world.
+    Just Close -> False
+    -- At medium distances, replace cell with static with a
+    -- probability that increases with distance.
+    Just (MidRange s) -> hp < 1 - cos (s * (pi / 2))
+    -- Far away, everything is static.
+    Just Far -> True
