swarm-game/swarm

commit a4c8057a28e043caed531e7d035efc2a41dc30a1

Records (#1148)
Add record types to the language: record values are written like `[x = 3, y = "hi"]` and have types like `[x : int, y : text]`.  Empty and singleton records are allowed.  You can project a field out of a record using standard dot notation, like `r.x`.  If things named e.g. `x` and `y` are in scope, you can also write e.g. `[x, y]` as a shorthand for `[x=x, y=y]`.

Closes #1093 .

#153 would make this even nicer to use.

One reason this is significant is that record projection is our first language construct whose type cannot be inferred, because if we see something like `r.x` all we know about the type of `r` is that it is a record type with at least one field `x`, but we don't know how many other fields it might have.  Without some complex stuff like row polymorphism we can't deal with that, so we just punt and throw an error saying that we can't infer the type of a projection.  To make this usable we have to do a better job checking types, a la #99 . For example `def f : [x:int] -> int = \r. r.x end` would not have type checked before, since when checking the lambda we immediately switched into inference mode, and then encountered the record projection and threw up our hands.  Now we work harder to push the given function type down into the lambda so that we are still in checking mode when we get to `r.x` which makes it work.  But it is probably easy to write examples of other things where this doesn't work.  Eventually we will want to fully implement #99 ; in the meantime one can always add a type annotation (#1164) on the record to get around this problem.

src/Swarm/Util.hs
@@ -16,6 +16,7 @@ module Swarm.Util (
   listEnums,
   uniq,
   binTuples,
+  findDup,
 
   -- * Directory utilities
   readFileMay,
@@ -24,6 +25,8 @@ module Swarm.Util (
   -- * Text utilities
   isIdentChar,
   replaceLast,
+  failT,
+  showT,
 
   -- * English language utilities
   reflow,
@@ -86,6 +89,7 @@ import NLP.Minimorph.English qualified as MM
 import NLP.Minimorph.Util ((<+>))
 import System.Clock (TimeSpec)
 import System.IO.Error (catchIOError)
+import Witch (from)
 
 infixr 1 ?
 infix 4 %%=, <+=, <%=, <<.=, <>=
@@ -145,6 +149,15 @@ binTuples = foldr f mempty
  where
   f = uncurry (M.insertWith (<>)) . fmap pure
 
+-- | Find a duplicate element within the list, if any exists.
+findDup :: Ord a => [a] -> Maybe a
+findDup = go S.empty
+ where
+  go _ [] = Nothing
+  go seen (a : as)
+    | a `S.member` seen = Just a
+    | otherwise = go (S.insert a seen) as
+
 ------------------------------------------------------------
 -- Directory stuff
 
@@ -184,6 +197,15 @@ isIdentChar c = isAlphaNum c || c == '_' || c == '\''
 -- "(move"
 replaceLast :: Text -> Text -> Text
 replaceLast r t = T.append (T.dropWhileEnd isIdentChar t) r
+
+-- | Fail with a Text-based message, made out of phrases to be joined
+--   by spaces.
+failT :: MonadFail m => [Text] -> m a
+failT = fail . from @Text . T.unlines
+
+-- | Show a value, but as Text.
+showT :: Show a => a -> Text
+showT = from @String . show
 
 ------------------------------------------------------------
 -- Some language-y stuff

data/entities.yaml
@@ -1193,3 +1193,33 @@
     inner workings.
   properties: [portable]
   capabilities: [debug]
+
+- name: victrola
+  display:
+    attr: device
+    char: 'Q'
+  description:
+  - |
+    A device for reading and writing data on circular platters made of
+    a soft plastic material.  The stylus must be made of a
+    hard and durable material, with a special tip that relies on
+    quantum effects to extract high-density information.
+  - |
+    Also allows manipulating composite values consisting of a
+    collection of named fields.  For example, `[x = 2, y = "hi"]`
+    is a value of type `[x : int, y : text]`.  Individual fields
+    can be projected using dot notation.  For example,
+    `let r = [y="hi", x=2] in r.x` has the value 2.  The order
+    of the fields does not matter.
+  properties: [portable]
+  capabilities: [record]
+
+- name: quantum dot
+  display:
+    attr: gold
+    char: '.'
+  description:
+  - |
+    A nanoscale semiconductor particle with a wide range of
+    applications.
+  properties: [portable]

data/recipes.yaml
@@ -808,3 +808,24 @@
   - [256, string]
   out:
   - [1, net]
+
+#########################################
+##                 MISC                ##
+#########################################
+
+- in:
+  - [1, water]
+  - [1, silicon]
+  required:
+  - [50, solar panel]
+  out:
+  - [1, quantum dot]
+
+- in:
+  - [1, small motor]
+  - [1, copper pipe]
+  - [1, mithril]
+  - [1, quantum dot]
+  - [1, iron plate]
+  out:
+  - [1, victrola]

feedback.yaml
@@ -1,2 +1,3 @@
 loops:
   test: stack test --fast
+  unit: stack test swarm:swarm-unit --fast

src/Swarm/Game/CESK.hs
@@ -86,7 +86,7 @@ import Data.Aeson (FromJSON, ToJSON)
 import Data.IntMap.Strict (IntMap)
 import Data.IntMap.Strict qualified as IM
 import GHC.Generics (Generic)
-import Prettyprinter (Doc, Pretty (..), hsep, (<+>))
+import Prettyprinter (Doc, Pretty (..), encloseSep, hsep, (<+>))
 import Swarm.Game.Entity (Count, Entity)
 import Swarm.Game.Exception
 import Swarm.Game.World (WorldUpdate (..))
@@ -169,6 +169,12 @@ data Frame
     --   nearby robots.  We have the function to run, and the list of
     --   robot IDs to run it on.
     FMeetAll Value [Int]
+  | -- | We are in the middle of evaluating a record: some fields have
+    --   already been evaluated; we are focusing on evaluating one
+    --   field; and some fields have yet to be evaluated.
+    FRcd Env [(Var, Value)] Var [(Var, Maybe Term)]
+  | -- | We are in the middle of evaluating a record field projection.(:*:)
+    FProj Var
   deriving (Eq, Show, Generic, FromJSON, ToJSON)
 
 -- | A continuation is just a stack of frames.
@@ -368,6 +374,14 @@ prettyFrame (FImmediate c _worldUpds _robotUpds) inner = prettyPrefix ("I[" <> p
 prettyFrame (FUpdate addr) inner = prettyPrefix ("S@" <> pretty addr) inner
 prettyFrame FFinishAtomic inner = prettyPrefix "A·" inner
 prettyFrame (FMeetAll _ _) inner = prettyPrefix "M·" inner
+prettyFrame (FRcd _ done foc rest) (_, inner) = (11, encloseSep "[" "]" ", " (pDone ++ [pFoc] ++ pRest))
+ where
+  pDone = map (\(x, v) -> pretty x <+> "=" <+> ppr (valueToTerm v)) (reverse done)
+  pFoc = pretty foc <+> "=" <+> inner
+  pRest = map pprEq rest
+  pprEq (x, Nothing) = pretty x
+  pprEq (x, Just t) = pretty x <+> "=" <+> ppr t
+prettyFrame (FProj x) (p, inner) = (11, pparens (p < 11) inner <> "." <> pretty x)
 
 -- | Pretty-print a special "prefix application" frame, i.e. a frame
 --   formatted like @X· inner@.  Unlike typical applications, these

src/Swarm/Game/Entity.hs
@@ -107,7 +107,7 @@ import Swarm.Game.Failure.Render (prettyFailure)
 import Swarm.Game.Location
 import Swarm.Game.ResourceLoading (getDataFileNameSafe)
 import Swarm.Language.Capability
-import Swarm.Util (binTuples, plural, reflow, (?))
+import Swarm.Util (binTuples, failT, plural, reflow, (?))
 import Swarm.Util.Yaml
 import Text.Read (readMaybe)
 import Witch
@@ -143,7 +143,7 @@ instance FromJSON EntityProperty where
     tryRead :: Text -> Parser EntityProperty
     tryRead t = case readMaybe . from . T.toTitle $ t of
       Just c -> return c
-      Nothing -> fail $ "Unknown entity property " ++ from t
+      Nothing -> failT ["Unknown entity property", t]
 
 -- | How long an entity takes to regrow.  This represents the minimum
 --   and maximum amount of time taken by one growth stage (there are
@@ -343,7 +343,7 @@ instance FromJSON Entity where
 instance FromJSONE EntityMap Entity where
   parseJSONE = withTextE "entity name" $ \name ->
     E $ \em -> case lookupEntityName name em of
-      Nothing -> fail $ "Unknown entity: " ++ from @Text name
+      Nothing -> failT ["Unknown entity:", name]
       Just e -> return e
 
 instance ToJSON Entity where

src/Swarm/Game/Scenario.hs
@@ -67,10 +67,11 @@ import Swarm.Game.Scenario.RobotLookup
 import Swarm.Game.Scenario.Style
 import Swarm.Game.Scenario.WorldDescription
 import Swarm.Language.Pipeline (ProcessedTerm)
+import Swarm.Util (failT)
 import Swarm.Util.Yaml
 import System.Directory (doesFileExist)
 import System.FilePath ((<.>), (</>))
-import Witch (from, into)
+import Witch (from)
 
 ------------------------------------------------------------
 -- Scenario
@@ -111,9 +112,7 @@ instance FromJSONE EntityMap Scenario where
       em' <- getE
       case filter (isNothing . (`lookupEntityName` em')) known of
         [] -> return ()
-        unk ->
-          fail . into @String $
-            "Unknown entities in 'known' list: " <> T.intercalate ", " unk
+        unk -> failT ["Unknown entities in 'known' list:", T.intercalate ", " unk]
 
       -- parse robots and build RobotMap
       rs <- v ..: "robots"

src/Swarm/Game/Scenario/Objective/Validation.hs
@@ -14,8 +14,7 @@ import Data.Set qualified as Set
 import Data.Text qualified as T
 import Swarm.Game.Scenario.Objective
 import Swarm.Game.Scenario.Objective.Graph
-import Swarm.Util (quote)
-import Witch (into)
+import Swarm.Util (failT, quote)
 
 -- | Performs monadic validation before returning
 -- the "pure" construction of a wrapper record.
@@ -31,21 +30,19 @@ validateObjectives objectives = do
   for_ objectives $ \x -> case _objectivePrerequisite x of
     Just p ->
       unless (null remaining) $
-        fail . into @String $
-          T.unwords
-            [ "Reference to undefined objective(s)"
-            , T.intercalate ", " (map quote $ Set.toList remaining) <> "."
-            , "Defined are:"
-            , T.intercalate ", " (map quote $ Set.toList allIds)
-            ]
+        failT
+          [ "Reference to undefined objective(s)"
+          , T.intercalate ", " (map quote $ Set.toList remaining) <> "."
+          , "Defined are:"
+          , T.intercalate ", " (map quote $ Set.toList allIds)
+          ]
      where
       refs = Set.fromList $ toList $ logic p
       remaining = Set.difference refs allIds
     Nothing -> return ()
 
   unless (isAcyclicGraph connectedComponents) $
-    fail . into @String $
-      T.unwords ["There are dependency cycles in the prerequisites."]
+    failT ["There are dependency cycles in the prerequisites."]
 
   return objectives
  where

commit ce923d5ddf5cee940abac5341bbe13d90ccb6cfe

actually join with spaces in `failT`, not newlines (#1179)
Fix bug introduced in #1148 .  See https://github.com/swarm-game/swarm/pull/1148#discussion_r1148410290

src/Swarm/Util.hs
@@ -201,7 +201,7 @@ replaceLast r t = T.append (T.dropWhileEnd isIdentChar t) r
 -- | Fail with a Text-based message, made out of phrases to be joined
 --   by spaces.
 failT :: MonadFail m => [Text] -> m a
-failT = fail . from @Text . T.unlines
+failT = fail . from @Text . T.unwords
 
 -- | Show a value, but as Text.
 showT :: Show a => a -> Text