1c771b9517b450c8ff1b7dad6c721f496fb17eee
Brent Yorgey <byorgey@gmail.com>
2023-04-16 19:52:45 -0500

    remove accidentally added file and remove completed todo

--- src/Swarm/Doc/Gen.hs
+++ src/Swarm/Doc/Gen.hs
@@ -66,8 +66,6 @@ import Text.Dot (Dot, NodeId, (.->.))
 import Text.Dot qualified as Dot
 import Witch (from)
 
--- XXX add keys cheatsheet
-
 -- ============================================================================
 -- MAIN ENTRYPOINT TO CLI DOCUMENTATION GENERATOR
 -- ============================================================================
