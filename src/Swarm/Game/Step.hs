ebe4bc196deaad41ea987ba8919cced039f27d5f
Brent Yorgey <byorgey@gmail.com>
2023-05-18 10:06:55 -0500

    ensure unprivileged robots can't halt privileged ones

--- src/Swarm/Game/Step.hs
+++ src/Swarm/Game/Step.hs
@@ -1694,11 +1694,18 @@ execConst c vs s k = do
           True -> return $ cancel $ Out VUnit s k
           False -> do
             -- Make sure the other robot exists and is close enough.
-            _other <- getRobotWithinTouch targetID
-            -- Cancel its CESK machine, and put it to sleep.
-            robotMap . at targetID . _Just . machine %= cancel
-            sleepForever targetID
-            return $ Out VUnit s k
+            target <- getRobotWithinTouch targetID
+            -- Make sure either we are privileged, OR the target robot
+            -- is NOT.  In other words unprivileged bots should not be
+            -- able to halt privileged ones.
+            omni <- isPrivilegedBot
+            case (omni || not (target ^. systemRobot)) of
+              True -> do
+                -- Cancel its CESK machine, and put it to sleep.
+                robotMap . at targetID . _Just . machine %= cancel
+                sleepForever targetID
+                return $ Out VUnit s k
+              False -> throwError $ cmdExn c ["You are not authorized to halt that robot."]
       _ -> badConst
     Ishere -> case vs of
       [VText name] -> do
