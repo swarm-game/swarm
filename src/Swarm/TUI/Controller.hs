b1b8952fde1eea755ebb270c03eafd5dd706f661
Karl Ostmo <kostmo@gmail.com>
2023-06-24 23:57:42 -0700

    suppress dialog

--- src/Swarm/TUI/Controller.hs
+++ src/Swarm/TUI/Controller.hs
@@ -859,7 +859,13 @@ updateUI = do
 
   goalOrWinUpdated <- doGoalUpdates
 
-  let redraw = g ^. needsRedraw || inventoryUpdated || replUpdated || logUpdated || infoPanelUpdated || goalOrWinUpdated
+  let redraw =
+           g ^. needsRedraw
+        || inventoryUpdated
+        || replUpdated
+        || logUpdated
+        || infoPanelUpdated
+        || goalOrWinUpdated
   pure redraw
 
 -- | Either pops up the updated Goals modal
@@ -942,7 +948,9 @@ doGoalUpdates = do
         -- automatically popped up.
         gameState . announcementQueue .= mempty
 
-        openModal GoalModal
+        isAutoplaying <- use $ uiState . uiIsAutoplay
+        unless isAutoplaying $
+          openModal GoalModal
 
       return goalWasUpdated
 
