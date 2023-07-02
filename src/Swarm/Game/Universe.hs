5dd0a19cc357aad1915ee6fb8f6ef52d8f9aea8e
Karl Ostmo <kostmo@gmail.com>
2023-07-01 23:59:24 -0700

    Add subworlds

---
+++ src/Swarm/Game/Universe.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+-- |
+-- SPDX-License-Identifier: BSD-3-Clause
+module Swarm.Game.Universe where
+
+import Control.Lens (makeLenses, view)
+import Control.Monad (guard)
+import Data.Function (on)
+import Data.Int (Int32)
+import Data.Text (Text)
+import Data.Yaml (FromJSON, ToJSON, Value (Object), parseJSON, (.:))
+import GHC.Generics (Generic)
+import Linear (V2 (..))
+import Swarm.Game.Location
+
+-- TODO: It would be better not to use this as
+-- an "in-band" reserved name. Preferably its use
+-- should be eliminated entirely.
+-- E.g., robot locations specified within a toplevel robot
+-- definition (instead of on a map) should require the subworld
+-- to be specified.  Currently, the subworld name is optional
+-- for backwards compatibility.
+-- If, after all, a "default" is still required, should
+-- use a dedicated sum type member to designate
+-- the "default" for cases in which it is required.
+defaultRootSubworldName :: SubworldName
+defaultRootSubworldName = SubworldName "root"
+
+-- | Note: The primary overworld shall use
+-- the reserved name \"root\".
+newtype SubworldName = SubworldName Text
+  deriving (Show, Eq, Ord, Generic, FromJSON, ToJSON)
+
+-- | The swarm universe consists of locations
+-- indexed by subworld.
+-- Not only is this datatype useful for planar (2D)
+-- coordinates, but is also used for named waypoints.
+data Cosmo a = Cosmo
+  { _subworld :: SubworldName
+  , _planar :: a
+  }
+  deriving (Show, Eq, Ord, Functor, Generic, ToJSON)
+
+makeLenses ''Cosmo
+
+instance (FromJSON a) => FromJSON (Cosmo a) where
+  parseJSON x = case x of
+    Object v -> objParse v
+    _ -> Cosmo defaultRootSubworldName <$> parseJSON x
+   where
+    objParse v =
+      Cosmo
+        <$> v .: "subworld"
+        <*> v .: "loc"
+
+defaultCosmoLocation :: Cosmo Location
+defaultCosmoLocation = Cosmo defaultRootSubworldName origin
+
+-- | Returns "Nothing" if not within the same subworld.
+-- TODO: Define a new datatype isomorphic to Maybe for this.
+cosmoMeasure :: (a -> a -> b) -> Cosmo a -> Cosmo a -> Maybe b
+cosmoMeasure f a b = do
+  guard $ ((==) `on` view subworld) a b
+  pure $ (f `on` view planar) a b
+
+offsetBy :: Cosmo Location -> V2 Int32 -> Cosmo Location
+offsetBy loc v = fmap (.+^ v) loc
--- src/Swarm/TUI/Controller.hs
+++ src/Swarm/TUI/Controller.hs
@@ -78,6 +78,7 @@ import Swarm.Game.Robot
 import Swarm.Game.ScenarioInfo
 import Swarm.Game.State
 import Swarm.Game.Step (finishGameTick, gameTick)
+import Swarm.Game.Universe
 import Swarm.Language.Capability (Capability (CDebug, CMake))
 import Swarm.Language.Context
 import Swarm.Language.Key (KeyCombo, mkKeyCombo)
@@ -377,6 +378,7 @@ handleMainEvent ev = do
       | s ^. uiState . uiCheatMode -> do
           uiState . uiWorldEditor . isWorldEditorEnabled %= not
           setFocus WorldEditorPanel
+    MouseDown WorldPositionIndicator _ _ _ -> uiState . uiWorldCursor .= Nothing
     MouseDown (FocusablePanel WorldPanel) V.BMiddle _ mouseLoc ->
       -- Eye Dropper tool
       EC.handleMiddleClick mouseLoc
@@ -394,7 +396,7 @@ handleMainEvent ev = do
       case n of
         FocusablePanel WorldPanel -> do
           mouseCoordsM <- Brick.zoom gameState $ mouseLocToWorldCoords mouseLoc
-          shouldUpdateCursor <- EC.updateAreaBounds mouseCoordsM
+          shouldUpdateCursor <- EC.updateAreaBounds $ fmap (^. planar) mouseCoordsM
           when shouldUpdateCursor $
             uiState . uiWorldCursor .= mouseCoordsM
         REPLInput -> handleREPLEvent ev
@@ -1340,7 +1342,7 @@ scrollView update = do
   -- always work, but there seems to be some sort of race condition
   -- where 'needsRedraw' gets reset before the UI drawing code runs.
   invalidateCacheEntry WorldCache
-  gameState %= modifyViewCenter update
+  gameState %= modifyViewCenter (fmap update)
 
 -- | Convert a directional key into a direction.
 keyToDir :: V.Key -> Heading
