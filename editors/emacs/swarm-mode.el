f817a36db460645ad544252c17590b013c6f9fe3
Karl Ostmo <kostmo@gmail.com>
2023-09-15 22:43:07 -0700

    'Be' command for unprivileged 'As'

--- editors/emacs/swarm-mode.el
+++ editors/emacs/swarm-mode.el
@@ -112,6 +112,7 @@
                "installkeyhandler"
                "teleport"
                "as"
+               "be"
                "robotnamed"
                "robotnumbered"
                "knows"
