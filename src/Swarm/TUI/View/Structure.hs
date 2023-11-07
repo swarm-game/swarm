dc7b7c75e21dc6e1db17200871ef9a309d6b1a61
Karl Ostmo <kostmo@gmail.com>
2023-11-07 09:49:19 -0800

    cosmetic improvement to structure browser

--- src/Swarm/TUI/View/Structure.hs
+++ src/Swarm/TUI/View/Structure.hs
@@ -19,6 +19,7 @@ import Data.Map.Strict qualified as M
 import Data.Text qualified as T
 import Data.Vector qualified as V
 import Swarm.Game.Entity (entityDisplay)
+import Swarm.Game.Scenario.Topography.Area
 import Swarm.Game.Scenario.Topography.Placement
 import Swarm.Game.Scenario.Topography.Structure qualified as Structure
 import Swarm.Game.Scenario.Topography.Structure.Recognition.Precompute (getEntityGrid)
@@ -31,31 +32,41 @@ import Swarm.TUI.Model.Structure
 import Swarm.TUI.View.Attribute.Attr
 import Swarm.TUI.View.CellDisplay
 import Swarm.TUI.View.Util
-import Swarm.Util (parens)
 
 structureWidget :: GameState -> StructureInfo -> Widget n
 structureWidget gs s =
-  hLimit 30 $
-    vBox
-      [ hBox
-          [ padRight (Pad 1) $ txt "Name:"
-          , withAttr boldAttr $ txt theName
-          , txt occurrenceCountSuffix
+  vBox
+    [ hBox
+        [ headerItem "Name" theName
+        , padLeft (Pad 2)
+            . headerItem "Size"
+            . T.pack
+            . renderRectDimensions
+            . getAreaDimensions
+            . entityGrid
+            $ withGrid s
+        , occurrenceCountSuffix
+        ]
+    , maybeDescriptionWidget
+    , padTop (Pad 1) $
+        hBox
+          [ structureIllustration
+          , padLeft (Pad 4) ingredientsBox
           ]
-      , maybeDescriptionWidget
-      , padTop (Pad 1) $
-          hBox
-            [ structureIllustration
-            , padLeft (Pad 4) ingredientsBox
-            ]
-      ]
+    ]
  where
+  headerItem h content =
+    hBox
+      [ padRight (Pad 1) $ txt $ h <> ":"
+      , withAttr boldAttr $ txt content
+      ]
+
   maybeDescriptionWidget = maybe emptyWidget txtWrap $ Structure.description . originalDefinition . withGrid $ s
 
   registry = gs ^. discovery . structureRecognition . foundStructures
   occurrenceCountSuffix = case M.lookup sName $ foundByName registry of
-    Nothing -> ""
-    Just inner -> " " <> parens (T.unwords [T.pack $ show $ NEM.size inner, "found"])
+    Nothing -> emptyWidget
+    Just inner -> padLeft (Pad 2) . headerItem "Count" . T.pack . show $ NEM.size inner
 
   structureIllustration = vBox $ map (hBox . map renderOneCell) cells
   d = originalDefinition $ withGrid s
@@ -79,7 +90,7 @@ structureWidget gs s =
 
   sName = Structure.name d
   StructureName theName = sName
-  cells = getEntityGrid d
+  cells = getEntityGrid $ Structure.structure d
   renderOneCell = maybe (txt " ") (renderDisplay . view entityDisplay)
 
 makeListWidget :: [StructureInfo] -> BL.List Name StructureInfo
