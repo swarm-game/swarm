3b439097713161b4f01ec5612c24492acddb6037
Karl Ostmo <kostmo@gmail.com>
2024-01-02 13:47:17 -0800

    bump cabal-build version, fix haddock script

--- scripts/view-haddocks.sh
+++ scripts/view-haddocks.sh
@@ -1,14 +1,11 @@
 #!/bin/bash -ex
 
-LOCAL_ARTIFACTS_DIR=.stack-work
-
 SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 cd $SCRIPT_DIR/..
 
-STACK_WORK=$LOCAL_ARTIFACTS_DIR stack haddock --fast
+cabal haddock
 
-DOCPATH_ROOT=$(STACK_WORK=$LOCAL_ARTIFACTS_DIR stack path --local-doc-root)
-SWARM_PACKAGE=$(stack ls dependencies --depth 0 swarm --separator -)
-SWARM_HADDOCK_INDEX=$DOCPATH_ROOT/$SWARM_PACKAGE/index.html
+OUTPUT_BASE_DIR=$(cabal list-bin swarm | grep -o '.*/x/' | head -c-3)
+SWARM_HADDOCK_INDEX=$OUTPUT_BASE_DIR/doc/html/swarm/index.html
 
 google-chrome $SWARM_HADDOCK_INDEX
