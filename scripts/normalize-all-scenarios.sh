ab9f86ee70489315a42cef7aeb170f9a39f9a054
Karl Ostmo <kostmo@gmail.com>
2024-01-05 11:32:26 -0800

    normalize scenarios (#1711)
    Closes #845
    
    **All changes are non-significant whitespace.**  This can be verified with:
    
        git show --ignore-all-space --ignore-blank-lines
    
    in which the remaining changes are only elimination of manual word-wrap in descriptions.
    
    Normalization is accomplished with this command:
    
        scripts/normalize-all-scenarios.sh
    
    This is an initial normalization pass that shall be a pre-requisite for #1713.

---
+++ scripts/normalize-all-scenarios.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR/..
+
+find data/scenarios -type f -name '*.yaml' -print0 | xargs -0 --max-args 1 yq --inplace
