3e9b7b8713b05e985b8ad3814f019e8640e5ed63
Brent Yorgey <byorgey@gmail.com>
2024-02-21 12:49:23 -0600

    better names in Subst

--- src/swarm-lang/Swarm/Language/Typecheck/Unify/Subst.hs
+++ src/swarm-lang/Swarm/Language/Typecheck/Unify/Subst.hs
@@ -8,11 +8,11 @@
 -- https://byorgey.wordpress.com/2021/09/08/implementing-hindley-milner-with-the-unification-fd-library/ .
 module Swarm.Language.Typecheck.Unify.Subst (
   -- * Substitutions
-  Substitution (..),
+  Subst (..),
   dom,
 
-  -- ** Subst type class
-  Subst (..),
+  -- ** Substitutes type class
+  Substitutes (..),
 
   -- ** Constructing/destructing substitutions
   idS,
@@ -33,7 +33,7 @@ import Data.Map qualified as M
 import Data.Set (Set)
 import Prelude hiding (lookup)
 
--- | A value of type @Substitution n a@ is a substitution which maps
+-- | A value of type @Subst n a@ is a substitution which maps
 --   names of type @n@ (the /domain/, see 'dom') to values of type
 --   @a@.  Substitutions can be /applied/ to certain terms (see
 --   'applySubst'), replacing any free occurrences of names in the
@@ -42,14 +42,14 @@ import Prelude hiding (lookup)
 --   @Term@s that contain names and values of the right type).
 --
 --   Concretely, substitutions are stored using a @Map@.
-newtype Substitution n a = Substitution {getSubst :: Map n a}
+newtype Subst n a = Subst {getSubst :: Map n a}
   deriving (Eq, Ord, Show)
 
-instance Functor (Substitution n) where
-  fmap f (Substitution m) = Substitution (M.map f $ m)
+instance Functor (Subst n) where
+  fmap f (Subst m) = Subst (M.map f $ m)
 
--- instance Pretty a => Pretty (Substitution a) where
---   pretty (Substitution s) = do
+-- instance Pretty a => Pretty (Subst a) where
+--   pretty (Subst s) = do
 --     let es = map (uncurry prettyMapping) (M.assocs s)
 --     ds <- punctuate "," es
 --     braces (hsep ds)
@@ -59,28 +59,30 @@ instance Functor (Substitution n) where
 
 -- | The domain of a substitution is the set of names for which the
 --   substitution is defined.
-dom :: Substitution n a -> Set n
+dom :: Subst n a -> Set n
 dom = M.keysSet . getSubst
 
 -- | The identity substitution, /i.e./ the unique substitution with an
 --   empty domain, which acts as the identity function on terms.
-idS :: Substitution n a
-idS = Substitution M.empty
+idS :: Subst n a
+idS = Subst M.empty
 
 -- | Construct a singleton substitution, which maps the given name to
 --   the given value.
-(|->) :: n -> a -> Substitution n a
-x |-> t = Substitution (M.singleton x t)
+(|->) :: n -> a -> Subst n a
+x |-> t = Subst (M.singleton x t)
 
--- | Class of things supporting substitution.  @Subst n b a@ means
---   that we can apply a substitution of type @Substitution n b@ to a
+-- | Class of things supporting substitution.  @Substitutes n b a@ means
+--   that we can apply a substitution of type @Subst n b@ to a
 --   value of type @a@, replacing all the free names of type @n@
 --   inside the @a@ with values of type @b@, resulting in a new value
 --   of type @a@.
-class Subst n b a where
-  subst :: Substitution n b -> a -> a
+class Substitutes n b a where
+  subst :: Subst n b -> a -> a
 
-instance (Ord n, Functor f) => Subst n (Free f n) (Free f n) where
+-- | We can perform substitution on terms built up as the free monad
+--   over a structure functor @f@.
+instance (Ord n, Functor f) => Substitutes n (Free f n) (Free f n) where
   subst = (=<<) . (!) . getSubst
 
 -- | Compose two substitutions.  Applying @s1 \@\@ s2@ is the same as
@@ -90,25 +92,25 @@ instance (Ord n, Functor f) => Subst n (Free f n) (Free f n) where
 --
 --   As one would expect, composition is associative and has 'idS' as
 --   its identity.
-(@@) :: (Ord n, Subst n a a) => Substitution n a -> Substitution n a -> Substitution n a
-(Substitution s1) @@ (Substitution s2) = Substitution ((M.map (subst (Substitution s1))) s2 `M.union` s1)
+(@@) :: (Ord n, Substitutes n a a) => Subst n a -> Subst n a -> Subst n a
+(Subst s1) @@ (Subst s2) = Subst ((M.map (subst (Subst s1))) s2 `M.union` s1)
 
 -- | Compose a whole container of substitutions.  For example,
 --   @compose [s1, s2, s3] = s1 \@\@ s2 \@\@ s3@.
-compose :: (Ord n, Subst n a a, Foldable t) => t (Substitution n a) -> Substitution n a
+compose :: (Ord n, Substitutes n a a, Foldable t) => t (Subst n a) -> Subst n a
 compose = foldr (@@) idS
 
 -- | Create a substitution from an association list of names and
 --   values.
-fromList :: Ord n => [(n, a)] -> Substitution n a
-fromList = Substitution . M.fromList
+fromList :: Ord n => [(n, a)] -> Subst n a
+fromList = Subst . M.fromList
 
 -- | Convert a substitution into an association list.
-toList :: Substitution n a -> [(n, a)]
+toList :: Subst n a -> [(n, a)]
 toList = M.assocs . getSubst
 
 -- | Look up the value a particular name maps to under the given
 --   substitution; or return @Nothing@ if the name being looked up is
 --   not in the domain.
-lookup :: Ord n => n -> Substitution n a -> Maybe a
-lookup x (Substitution m) = M.lookup x m
+lookup :: Ord n => n -> Subst n a -> Maybe a
+lookup x (Subst m) = M.lookup x m
