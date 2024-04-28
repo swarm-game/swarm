028f3adb29e53a936f1aa5557de2325ab4c03c96
Brent Yorgey <byorgey@gmail.com>
2024-04-28 17:09:42 -0500

    fix up `Web.Tournament.Validate` imports

--- src/swarm-tournament/Swarm/Web/Tournament/Validate.hs
+++ src/swarm-tournament/Swarm/Web/Tournament/Validate.hs
@@ -13,41 +13,29 @@ import Control.Carrier.Throw.Either (runThrow)
 import Control.Lens
 import Control.Monad (unless)
 import Control.Monad.IO.Class (liftIO)
-import Control.Monad.State (StateT, evalStateT, gets)
+import Control.Monad.State (evalStateT)
 import Control.Monad.Trans.Except
 import Data.ByteString.Lazy qualified as LBS
 import Data.Either.Extra (maybeToEither)
-import Data.Foldable (Foldable (toList))
-import Data.List.NonEmpty qualified as NE
 import Data.Sequence (Seq)
 import Data.Text qualified as T
 import Data.Text.Encoding (decodeUtf8')
 import Data.Yaml (decodeEither', parseEither)
 import Servant.Multipart
-import Swarm.Effect (runTimeIO)
 import Swarm.Game.CESK (emptyStore, initMachine)
 import Swarm.Game.Failure (SystemFailure)
-import Swarm.Game.Robot.Concrete (machine, robotContext, robotLog)
+import Swarm.Game.Robot.Concrete (machine, robotContext)
 import Swarm.Game.Robot.Context (defReqs)
 import Swarm.Game.Scenario
 import Swarm.Game.Scenario.Scoring.CodeSize (codeMetricsFromSyntax)
 import Swarm.Game.Scenario.Status (emptyLaunchParams)
 import Swarm.Game.State
-import Swarm.Game.State.Robot (robotMap)
 import Swarm.Game.State.Runtime (initGameStateConfig, initScenarioInputs)
-import Swarm.Game.State.Substate (
-  WinCondition (WinConditions),
-  WinStatus (Won),
-  initState,
-  messageQueue,
-  seed,
- )
-import Swarm.Game.Step (gameTick)
-import Swarm.Game.Tick (TickNumber)
+import Swarm.Game.State.Substate (initState, seed)
+import Swarm.Game.Step.Validate (playUntilWin)
 import Swarm.Language.Context qualified as Ctx
 import Swarm.Language.Module (Module (..))
 import Swarm.Language.Pipeline (ProcessedTerm (..), processTermEither)
-import Swarm.Log
 import Swarm.Util.Yaml
 import Swarm.Web.Tournament.Database.Query
 import Swarm.Web.Tournament.Type
