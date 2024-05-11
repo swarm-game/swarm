d14600c10e5396947ba65cca765bdb59bafcb96d
Brent Yorgey <byorgey@gmail.com>
2024-05-11 16:35:04 -0500

    WIP: start splitting out Language.Parser.Lex module

---
+++ src/swarm-lang/Swarm/Language/Parser/Lex.hs
@@ -0,0 +1,150 @@
+-- |
+-- SPDX-License-Identifier: BSD-3-Clause
+--
+-- Token lexing and comment preservation for the Swarm language.
+module Swarm.Language.Parser.Lex (
+  reservedWords,
+  sc,
+  lexeme,
+  symbol,
+  reserved,
+  identifier,
+  locIdentifier,
+  textLiteral,
+  integer,
+  braces,
+  parens,
+) where
+
+-- | List of reserved words that cannot be used as variable names.
+reservedWords :: [Text]
+reservedWords =
+  map (syntax . constInfo) (filter isUserFunc allConst)
+    ++ map directionSyntax allDirs
+    ++ [ "void"
+       , "unit"
+       , "int"
+       , "text"
+       , "dir"
+       , "bool"
+       , "actor"
+       , "key"
+       , "cmd"
+       , "delay"
+       , "let"
+       , "def"
+       , "end"
+       , "in"
+       , "true"
+       , "false"
+       , "forall"
+       , "require"
+       , "requirements"
+       ]
+
+-- Approach for preserving comments taken from https://www.reddit.com/r/haskell/comments/ni4gpm/comment/gz0ipmp/
+
+-- | If we see a comment starting now, is it the first non-whitespace
+--   thing on the current line so far, or were there other
+--   non-whitespace tokens previously?
+getCommentSituation :: Parser CommentSituation
+getCommentSituation = do
+  fl <- use freshLine
+  return $ if fl then StandaloneComment else SuffixComment
+
+-- | Parse a line comment, while appending it out-of-band to the list of
+--   comments saved in the custom state.
+lineComment :: Text -> Parser ()
+lineComment start = do
+  cs <- getCommentSituation
+  (loc, t) <- parseLocG $ do
+    string start *> takeWhileP (Just "character") (/= '\n')
+  comments %= (Seq.|> Comment loc LineComment cs t)
+
+-- | Parse a block comment, while appending it out-of-band to the list of
+--   comments saved in the custom state.
+blockComment :: Text -> Text -> Parser ()
+blockComment start end = do
+  cs <- getCommentSituation
+  (loc, t) <- parseLocG $ do
+    void $ string start
+    manyTill anySingle (string end)
+  comments %= (Seq.|> Comment loc BlockComment cs (fromString t))
+
+-- | Skip spaces and comments.
+sc :: Parser ()
+sc =
+  -- Typically we would use L.space here, but we have to inline its
+  -- definition and use our own slight variant, since we need to treat
+  -- end-of-line specially.
+  skipMany . choice . map hidden $
+    [ hspace1
+    , eol *> (freshLine .= True) -- If we see a newline, reset freshLine to True.
+    , lineComment "//"
+    , blockComment "/*" "*/"
+    ]
+
+-- | In general, we follow the convention that every token parser
+--   assumes no leading whitespace and consumes all trailing
+--   whitespace.  Concretely, we achieve this by wrapping every token
+--   parser using 'lexeme'.
+--
+--   Also sets freshLine to False every time we see a non-whitespace
+--   token.
+lexeme :: Parser a -> Parser a
+lexeme p = (freshLine .= False) *> L.lexeme sc p
+
+-- | A lexeme consisting of a literal string.
+symbol :: Text -> Parser Text
+symbol s = (freshLine .= False) *> L.symbol sc s
+
+-- | Parse a case-insensitive reserved word, making sure it is not a
+--   prefix of a longer variable name, and allowing the parser to
+--   backtrack if it fails.
+reserved :: Text -> Parser ()
+reserved w = (lexeme . try) $ string' w *> notFollowedBy (alphaNumChar <|> char '_')
+
+-- | Parse an identifier, i.e. any non-reserved string containing
+--   alphanumeric characters and underscores and not starting with a
+--   number.
+identifier :: Parser Var
+identifier = lvVar <$> locIdentifier
+
+-- | Parse an identifier together with its source location info.
+locIdentifier :: Parser LocVar
+locIdentifier = uncurry LV <$> parseLocG ((lexeme . try) (p >>= check) <?> "variable name")
+ where
+  p = (:) <$> (letterChar <|> char '_') <*> many (alphaNumChar <|> char '_' <|> char '\'')
+  check (into @Text -> t)
+    | toLower t `elem` reservedWords =
+        failT ["reserved word", squote t, "cannot be used as variable name"]
+    | otherwise = return t
+
+-- | Parse a text literal (including escape sequences) in double quotes.
+textLiteral :: Parser Text
+textLiteral = into <$> lexeme (char '"' >> manyTill L.charLiteral (char '"'))
+
+-- | Parse a positive integer literal token, in decimal, binary,
+--   octal, or hexadecimal notation.  Note that negation is handled as
+--   a separate operator.
+integer :: Parser Integer
+integer =
+  label "integer literal" $
+    lexeme $ do
+      n <-
+        string "0b" *> L.binary
+          <|> string "0o" *> L.octal
+          <|> string "0x" *> L.hexadecimal
+          <|> L.decimal
+      notFollowedBy alphaNumChar
+      return n
+
+braces :: Parser a -> Parser a
+braces = between (symbol "{") (symbol "}")
+
+parens :: Parser a -> Parser a
+parens = between (symbol "(") (symbol ")")
+
+brackets :: Parser a -> Parser a
+brackets = between (symbol "[") (symbol "]")
+
