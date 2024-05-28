508c099c4013f98a14b4c5f40a9f66d4a722da68
Brent Yorgey <byorgey@gmail.com>
2024-05-27 22:10:53 -0500

    Update `FromJSON TickNumber` instance to match `ToJSON` instance (#1870)
    Fixes #1868.

--- src/swarm-engine/Swarm/Game/Tick.hs
+++ src/swarm-engine/Swarm/Game/Tick.hs
@@ -12,15 +12,19 @@ import Data.Aeson qualified as A
 import Data.Int (Int64)
 import GHC.Generics (Generic)
 import Prettyprinter (Pretty (..))
+import Swarm.Util.JSON (optionsUnwrapUnary)
 import Swarm.Util.WindowedCounter (Offsettable (..))
 
 -- | A newtype representing a count of ticks (typically since the
 --   start of a game).
 newtype TickNumber = TickNumber {getTickNumber :: Int64}
-  deriving (Eq, Ord, Show, Read, Generic, FromJSON)
+  deriving (Eq, Ord, Show, Read, Generic)
 
 instance ToJSON TickNumber where
-  toJSON = A.genericToJSON (A.defaultOptions {A.unwrapUnaryRecords = True})
+  toJSON = A.genericToJSON optionsUnwrapUnary
+
+instance FromJSON TickNumber where
+  parseJSON = A.genericParseJSON optionsUnwrapUnary
 
 -- | Add an offset to a 'TickNumber'.
 addTicks :: Int -> TickNumber -> TickNumber
