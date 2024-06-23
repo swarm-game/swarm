eedf6d201c506e3c4120e057418929cc6d6cb2d0
Noah Yorgey <noah.yorgey@gmail.com>
2024-06-23 14:43:39 -0500

    Add more description

--- src/swarm-tui/Swarm/TUI/Model/Name.hs
+++ src/swarm-tui/Swarm/TUI/Model/Name.hs
@@ -112,6 +112,6 @@ data Name
     REPLViewport
   | -- | A clickable button in a modal dialog.
     Button Button
-  | -- | A custom widget name.
+  | -- | A custom widget name, for use in applications built on top of the Swarm library.
     CustomName Text
   deriving (Eq, Ord, Show, Read)
