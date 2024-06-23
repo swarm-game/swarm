3b7eaeacebc03e233d90969e76e82ea4b7a22440
Noah Yorgey <noah.yorgey@gmail.com>
2024-06-23 14:36:56 -0500

    Add Custom Widget Names

--- src/swarm-tui/Swarm/TUI/Model/Name.hs
+++ src/swarm-tui/Swarm/TUI/Model/Name.hs
@@ -8,6 +8,8 @@
 -- case matching.
 module Swarm.TUI.Model.Name where
 
+import Data.Text (Text)
+
 data WorldEditorFocusable
   = BrushSelector
   | EntitySelector
@@ -110,4 +112,6 @@ data Name
     REPLViewport
   | -- | A clickable button in a modal dialog.
     Button Button
+  | -- | A custom widget name
+    CustomName Text
   deriving (Eq, Ord, Show, Read)
