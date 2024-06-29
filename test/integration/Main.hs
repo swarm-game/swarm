8943f98fad319d10384e578539e5661bfa7dae5c
Ondřej Šebek <ondras98@icloud.com>
2024-06-29 21:37:13 +0200

    Fixup tests

--- test/integration/Main.hs
+++ test/integration/Main.hs
@@ -72,7 +72,7 @@ import Swarm.TUI.Model (
   defaultAppOpts,
   gameState,
   runtimeState,
-  userScenario,
+  userScenario, KeyEventHandlingState,
  )
 import Swarm.TUI.Model.StateUpdate (constructAppState, initPersistentState)
 import Swarm.TUI.Model.UI (UIState)
@@ -95,7 +95,7 @@ main = do
   scenarioPaths <- findAllWithExt "data/scenarios" "yaml"
   let (unparseableScenarios, parseableScenarios) = partition isUnparseableTest scenarioPaths
   scenarioPrograms <- findAllWithExt "data/scenarios" "sw"
-  (rs, ui) <- do
+  (rs, ui, key) <- do
     out <- runM . runThrow @SystemFailure $ initPersistentState defaultAppOpts
     either (assertFailure . prettyString) return out
   let scenarioInputs = gsiScenarioInputs $ initState $ rs ^. stdGameConfigInputs
@@ -108,7 +108,7 @@ main = do
       , exampleTests scenarioPrograms
       , scenarioParseTests scenarioInputs parseableScenarios
       , scenarioParseInvalidTests scenarioInputs unparseableScenarios
-      , testScenarioSolutions rs' ui
+      , testScenarioSolutions rs' ui key
       , testEditorFiles
       ]
 
@@ -183,8 +183,8 @@ time = \case
 
 data ShouldCheckBadErrors = CheckForBadErrors | AllowBadErrors deriving (Eq, Show)
 
-testScenarioSolutions :: RuntimeState -> UIState -> TestTree
-testScenarioSolutions rs ui =
+testScenarioSolutions :: RuntimeState -> UIState -> KeyEventHandlingState -> TestTree
+testScenarioSolutions rs ui key =
   testGroup
     "Test scenario solutions"
     [ testGroup
@@ -480,7 +480,7 @@ testScenarioSolutions rs ui =
 
   testSolution' :: Time -> FilePath -> ShouldCheckBadErrors -> (GameState -> Assertion) -> TestTree
   testSolution' s p shouldCheckBadErrors verify = testCase p $ do
-    out <- runM . runThrow @SystemFailure $ constructAppState rs ui $ defaultAppOpts {userScenario = Just p}
+    out <- runM . runThrow @SystemFailure $ constructAppState rs ui key $ defaultAppOpts {userScenario = Just p}
     case out of
       Left err -> assertFailure $ prettyString err
       Right appState -> case appState ^. gameState . winSolution of
