28a899a0d47154485b09322e2eea84828c0de521
Ondřej Šebek <ondras98@icloud.com>
2024-06-29 20:10:09 +0200

    Move world panel events to new handler

---
+++ src/swarm-tui/Swarm/TUI/Controller/EventHandlers/World.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- |
+-- SPDX-License-Identifier: BSD-3-Clause
+--
+-- TODO: describe
+module Swarm.TUI.Controller.EventHandlers.World (
+  worldEventHandlers,
+) where
+
+import Brick hiding (Location)
+import Brick.Keybindings
+import Control.Lens
+import Control.Monad (when)
+import Data.Int (Int32)
+import Linear
+import Swarm.Game.Location
+import Swarm.Game.State
+import Swarm.Game.State.Landscape
+import Swarm.Game.State.Robot
+import Swarm.TUI.Controller.Util
+import Swarm.TUI.Model
+import Swarm.TUI.Model.Event
+import Swarm.TUI.Model.UI
+
+-- | Handle a user input event in the world view panel.
+worldEventHandlers :: [KeyEventHandler SwarmEvent (EventM Name AppState)]
+worldEventHandlers = allHandlers World $ \case
+  ViewBaseEvent -> ("View the base robot", viewBase)
+  ShowFpsEvent -> ("Show frames per second", showFps)
+  MoveViewNorthEvent -> ("Scroll world view in the north direction", scrollViewInDir north)
+  MoveViewEastEvent -> ("Scroll world view in the east direction", scrollViewInDir east)
+  MoveViewSouthEvent -> ("Scroll world view in the south direction", scrollViewInDir south)
+  MoveViewWestEvent -> ("Scroll world view in the west direction", scrollViewInDir west)
+
+viewBase :: EventM Name AppState ()
+viewBase = do
+  invalidateCacheEntry WorldCache
+  gameState . robotInfo . viewCenterRule .= VCRobot 0
+
+showFps :: EventM Name AppState ()
+showFps = uiState . uiGameplay . uiTiming . uiShowFPS %= not
+
+scrollViewInDir :: V2 Int32 -> EventM Name AppState ()
+scrollViewInDir d = do
+  c <- use $ gameState . creativeMode
+  s <- use $ gameState . landscape . worldScrollable
+  when (c || s) $ scrollView (.+^ (worldScrollDist *^ d))
+
+worldScrollDist :: Int32
+worldScrollDist = 8
+
+-- | Manually scroll the world view.
+scrollView :: (Location -> Location) -> EventM Name AppState ()
+scrollView update = do
+  -- Manually invalidate the 'WorldCache' instead of just setting
+  -- 'needsRedraw'.  I don't quite understand why the latter doesn't
+  -- always work, but there seems to be some sort of race condition
+  -- where 'needsRedraw' gets reset before the UI drawing code runs.
+  invalidateCacheEntry WorldCache
+  gameState . robotInfo %= modifyViewCenter (fmap update)
