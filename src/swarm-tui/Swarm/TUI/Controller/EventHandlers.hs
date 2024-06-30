35905fbccba9d340728cea65acb6a658b93e559e
Ondřej Šebek <ondras98@icloud.com>
2024-06-30 15:55:38 +0200

    Handle conflict between dispatchers
    For example:
    ```bash
    $ cat ~/.config/swarm/config.ini
    [keybindings]
    show zero inventory entites = f1
    $ cabal run swarm -O0 -- keybindings
    Error: some key events have keys bound to them in 'Main game events' and in 'Robot panel events'
    Handlers with the 'F1' binding:
      View Help screen (triggered by the event 'view help')
      Show entities with zero count in inventory (triggered by the event 'show zero inventory entites')
    ```

--- src/swarm-tui/Swarm/TUI/Controller/EventHandlers.hs
+++ src/swarm-tui/Swarm/TUI/Controller/EventHandlers.hs
@@ -4,11 +4,20 @@
 -- |
 -- SPDX-License-Identifier: BSD-3-Clause
 --
--- Sum types representing the Swarm events
--- abstracted away from keybindings.
+-- Swarm (abstract) event handlers allow players to customize some keybindings.
+-- This all comes together in 'Swarm.TUI.Controller' which calls the handlers
+-- for parts of UI and also handles mouse events and frame updates.
+--
+-- The high level overview is this:
+-- 1. The 'SwarmEvent' is a enumeration of abstracts key events ('PauseEvent', etc.)
+-- 2. The 'AppState' stores the key configuration and key dispatchers (keys to handlers)
+-- 3. Here we declare the handlers for abstract events.
+-- 4. When provided with 'KeyConfig' (can include customized keybindings) we can
+--    'createKeyDispatchers in 'Swarm.TUI.Model.StateUpdate' and store them in 'AppState'.
+-- 5. Finally in 'Swarm.TUI.Controller' the Brick event handler calls the stored dispatchers.
 module Swarm.TUI.Controller.EventHandlers (
   -- * Documentation
-  createEventHandlers,
+  createKeyDispatchers,
 
   -- ** Main game handler
   mainEventHandlers,
@@ -47,36 +56,50 @@ import Swarm.TUI.Controller.EventHandlers.World (worldEventHandlers)
 import Swarm.TUI.Model
 import Swarm.TUI.Model.Event (SwarmEvent, swarmEvents)
 
--- | Create event handlers with given key config.
+-- | Create key dispatchers that call (abstract) event handlers based on given key config.
 --
--- Fails if any key events have conflict within one dispatcher.
-createEventHandlers ::
+-- Fails if any key events have conflict within one dispatcher or when a main dispatcher
+-- has conflict with one of the subdispatchers.
+createKeyDispatchers ::
   (Has (Throw SystemFailure) sig m) =>
   KeyConfig SwarmEvent ->
   m SwarmKeyDispatchers
-createEventHandlers config = do
+createKeyDispatchers config = do
   mainGameDispatcher <- buildDispatcher mainEventHandlers
-  replDispatcher <- buildDispatcher replEventHandlers
-  worldDispatcher <- buildDispatcher worldEventHandlers
-  robotDispatcher <- buildDispatcher robotEventHandlers
+  let buildSubMainDispatcher = buildSubDispatcher "Main game events" mainGameDispatcher
+  replDispatcher <- buildSubMainDispatcher "REPL panel events" replEventHandlers
+  worldDispatcher <- buildSubMainDispatcher "World panel events" worldEventHandlers
+  robotDispatcher <- buildSubMainDispatcher "Robot panel events" robotEventHandlers
   return SwarmKeyDispatchers {..}
  where
   -- this error handling code is modified version of the brick demo app:
   -- https://github.com/jtdaugherty/brick/blob/764e66897/programs/CustomKeybindingDemo.hs#L216
   buildDispatcher handlers = case keyDispatcher config handlers of
+    Left collisions ->
+      throwLoadingFailure $
+        "Error: some key events have the same keys bound to them.\n"
+          : handlerErrors collisions
     Right d -> return d
-    Left collisions -> do
-      let errorHeader = "Error: some key events have the same keys bound to them.\n"
-      let handlerErrors = flip map collisions $ \(b, hs) ->
-            let hsm = "Handlers with the '" <> BK.ppBinding b <> "' binding:"
-                hss = flip map hs $ \h ->
-                  let trigger = case BK.kehEventTrigger $ BK.khHandler h of
-                        ByKey k -> "triggered by the key '" <> BK.ppBinding k <> "'"
-                        ByEvent e -> "triggered by the event '" <> fromMaybe "<unknown>" (BK.keyEventName swarmEvents e) <> "'"
-                      desc = BK.handlerDescription $ BK.kehHandler $ BK.khHandler h
-                   in "  " <> desc <> " (" <> trigger <> ")"
-             in T.intercalate "\n" (hsm : hss)
-      throwError $ CustomFailure (T.intercalate "\n" $ errorHeader : handlerErrors)
+  buildSubDispatcher parentName parentDispatcher name handlers = do
+    d <- buildDispatcher handlers
+    let collisions = conflicts parentDispatcher d
+    if null collisions
+      then return d
+      else
+        throwLoadingFailure $
+          ("Error: some key events have keys bound to them in '" <> parentName <> "' and in '" <> name <> "'")
+            : handlerErrors collisions
+
+  throwLoadingFailure = throwError . CustomFailure . T.intercalate "\n"
+  handlerErrors collisions = flip map collisions $ \(b, hs) ->
+    let hsm = "Handlers with the '" <> BK.ppBinding b <> "' binding:"
+        hss = flip map hs $ \h ->
+          let trigger = case BK.kehEventTrigger $ BK.khHandler h of
+                ByKey k -> "triggered by the key '" <> BK.ppBinding k <> "'"
+                ByEvent e -> "triggered by the event '" <> fromMaybe "<unknown>" (BK.keyEventName swarmEvents e) <> "'"
+              desc = BK.handlerDescription $ BK.kehHandler $ BK.khHandler h
+           in "  " <> desc <> " (" <> trigger <> ")"
+     in T.intercalate "\n" (hsm : hss)
 
 -- | Take two dispatchers (that do not have conflict themselves) and find conflicting keys between them.
 conflicts :: SwarmKeyDispatcher -> SwarmKeyDispatcher -> [(Binding, [KeyHandler SwarmEvent (EventM Name AppState)])]
