5631067e1a79ccc7414dd1afaac6016daa8d0647
Brent Yorgey <byorgey@gmail.com>
2024-07-12 20:48:09 -0500

    add some comments

--- src/swarm-tui/Swarm/TUI/View/Notification.hs
+++ src/swarm-tui/Swarm/TUI/View/Notification.hs
@@ -1,4 +1,7 @@
--- XXX comment me
+-- |
+-- SPDX-License-Identifier: BSD-3-Clause
+--
+-- Rendering (& animating) notification popup.
 module Swarm.TUI.View.Notification where
 
 import Brick (Widget (..), cropTopTo, padLeftRight, txt)
@@ -10,9 +13,12 @@ import Swarm.TUI.Model (AppState, Name, uiState)
 import Swarm.TUI.Model.Notification (currentNotification, notificationFrames)
 import Swarm.TUI.Model.UI (uiNotifications)
 
+-- | The number of frames taken by each step of the notification popup
+--   animation.
 animFrames :: Int
 animFrames = 3
 
+-- | Draw the current notification popup (if any).
 drawNotifications :: AppState -> Widget Name
 drawNotifications s = hCenterLayer $
   case s ^. uiState . uiNotifications . currentNotification of
@@ -25,5 +31,14 @@ drawNotifications s = hCenterLayer $
 --   This is what causes the popup to animate in and out of existence.
 notificationRows :: Int -> Int
 notificationRows f
+  -- If we're less than halfway through the lifetime of the popup,
+  -- divide the number of frames by the number of frames for each step
+  -- of the animation (rounded up).  This will become much larger than
+  -- the actual number of rows in the popup, but the 'cropTopTo' function
+  -- simply has no effect when given any value equal to or larger than the
+  -- number of rows of a widget.  This way the animation will continue to
+  -- work for popups with any (reasonable) number of rows.
   | f <= notificationFrames `div` 2 = (f + animFrames - 1) `div` animFrames
+  -- Otherwise, divide the number of frames remaining by the number of
+  -- frames for each step of the animation (rounded up).
   | otherwise = (notificationFrames - f + animFrames - 1) `div` animFrames
