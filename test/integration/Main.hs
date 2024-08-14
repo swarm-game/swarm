d370cde52effd2bf8b028a3f5e06e64975ed5892
Karl Ostmo <kostmo@gmail.com>
2024-08-13 23:44:24 -0700

    add tests

--- test/integration/Main.hs
+++ test/integration/Main.hs
@@ -454,6 +454,8 @@ testScenarioSolutions rs ui key =
             , testSolution Default "Testing/1575-structure-recognizer/1575-bounding-box-overlap"
             , testSolution Default "Testing/1575-structure-recognizer/1644-rotated-recognition"
             , testSolution Default "Testing/1575-structure-recognizer/1644-rotated-preplacement-recognition"
+            , testSolution Default "Testing/1575-structure-recognizer/2115-encroaching-upon-interior-transparent-cells"
+            , testSolution Default "Testing/1575-structure-recognizer/2115-encroaching-upon-exterior-transparent-cells"
             ]
         ]
     , testSolution' Default "Testing/1430-built-robot-ownership" CheckForBadErrors $ \g -> do
