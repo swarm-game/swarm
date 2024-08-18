3c7719161f74fbe135f5864692883f6eec921004
Karl Ostmo <kostmo@gmail.com>
2024-08-18 15:33:24 -0700

    Pied piper scenario

--- test/integration/Main.hs
+++ test/integration/Main.hs
@@ -263,6 +263,7 @@ testScenarioSolutions rs ui key =
             , testSolution (Sec 20) "Challenges/Ranching/powerset"
             , testSolution (Sec 10) "Challenges/Ranching/fishing"
             , testSolution (Sec 30) "Challenges/Ranching/gated-paddock"
+            , testSolution (Sec 30) "Challenges/Ranching/pied-piper"
             ]
         , testGroup
             "Sokoban"
