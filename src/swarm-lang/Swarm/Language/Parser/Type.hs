27a3b1f662250cde686814f89bb60a34d9d6fdc8
Brent Yorgey <byorgey@gmail.com>
2024-10-13 14:03:32 -0500

    no longer deal with implicit quantification during parsing

--- src/swarm-lang/Swarm/Language/Parser/Type.hs
+++ src/swarm-lang/Swarm/Language/Parser/Type.hs
@@ -42,26 +42,9 @@ import Witch (from)
 --   'parseType' is also accepted by 'parsePolytype'.
 parsePolytype :: Parser Polytype
 parsePolytype =
-  join $
-    ( quantify . fromMaybe []
-        <$> optional ((reserved "forall" <|> reserved "∀") *> some tyVar <* symbol ".")
-    )
-      <*> parseType
- where
-  quantify :: [Var] -> Type -> Parser Polytype
-  quantify xs ty
-    -- Iplicitly quantify over free type variables if the user didn't write a forall
-    | null xs = return $ Forall (S.toList free) ty
-    -- Otherwise, require all variables to be explicitly quantified
-    | S.null free = return $ Forall xs ty
-    | otherwise =
-        fail $
-          unlines
-            [ "  Type contains free variable(s): " ++ unwords (map from (S.toList free))
-            , "  Try adding them to the 'forall'."
-            ]
-   where
-    free = tyVars ty `S.difference` S.fromList xs
+  Forall . fromMaybe []
+    <$> optional ((reserved "forall" <|> reserved "∀") *> some tyVar <* symbol ".")
+    <*> parseType
 
 -- | Parse a Swarm language (mono)type.
 parseType :: Parser Type
