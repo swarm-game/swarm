bbd81e80348c4f5613b9d03a55e35edf231cc157
Brent Yorgey <byorgey@gmail.com>
2024-12-26 12:15:33 -0600

    fix warnings, style, + add TODO re: `read` for other types

--- src/swarm-lang/Swarm/Language/Parser/Value.hs
+++ src/swarm-lang/Swarm/Language/Parser/Value.hs
@@ -1,13 +1,9 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeApplications #-}
-
 -- |
 -- SPDX-License-Identifier: BSD-3-Clause
 --
 -- Parse values of the Swarm language, indexed by type, by running the
 -- full swarm-lang parser and then checking that the result is a value
 -- of the proper type.
---
 module Swarm.Language.Parser.Value (readValue) where
 
 import Control.Lens ((^.))
@@ -15,7 +11,7 @@ import Data.Either.Extra (eitherToMaybe)
 import Data.Text (Text)
 import Swarm.Language.Context qualified as Ctx
 import Swarm.Language.Key (parseKeyComboFull)
-import Swarm.Language.Parser
+import Swarm.Language.Parser (readNonemptyTerm)
 import Swarm.Language.Syntax
 import Swarm.Language.Typecheck (checkTop)
 import Swarm.Language.Types (Type)
@@ -45,9 +41,10 @@ toValue = \case
     _ -> Nothing
   TPair t1 t2 -> VPair <$> toValue t1 <*> toValue t2
   TRcd m -> VRcd <$> traverse (>>= toValue) m
-
+  -- List the other cases explicitly, instead of a catch-all, so that
+  -- we will get a warning if we ever add new constructors in the
+  -- future
   TConst {} -> Nothing
-  -- TConst k -> Just $ VCApp k []   -- XXX for parsing commands
   TAntiInt {} -> Nothing
   TAntiText {} -> Nothing
   TRequireDevice {} -> Nothing
@@ -55,21 +52,18 @@ toValue = \case
   TRequirements {} -> Nothing
   TVar {} -> Nothing
   TLam {} -> Nothing
-  TApp {} -> Nothing  -- XXX command values?
-    -- TApp t1 t2 -> case toValue t1 of
-    --   Just (VCApp c vs) -> VCApp c . (:vs) <$> toValue t2  -- XXX reverse?
-    --   _ -> Nothing
-    -- Would need to also check to make sure isCmd is true for an application
-    --   of the constant to be a value?
-    -- No, we need this to handle sum types!!
-
-  TLet {} -> Nothing  -- XXX closures?
+  TApp {} -> Nothing
+  TLet {} -> Nothing
   TTydef {} -> Nothing
-  TBind {} -> Nothing  -- XXX command value?
-  TDelay {} -> Nothing  -- XXX VDelay?
+  TBind {} -> Nothing
+  TDelay {} -> Nothing
   TProj {} -> Nothing
   TAnnotate {} -> Nothing
   TSuspend {} -> Nothing
+
+-- TODO(#2232): in order to get `read` to work for delay, function,
+-- and/or command types, we will need to handle a few more of the
+-- above cases, e.g. TConst, TLam, TApp, TLet, TBind, TDelay.
 
 negateInt :: Value -> Maybe Value
 negateInt = \case
