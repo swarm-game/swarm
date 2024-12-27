14ed1fd21e766fc222b881f989649342964a9b26
Brent Yorgey <byorgey@gmail.com>
2024-12-27 09:49:57 -0600

    store validated Kolor inside HexColor instead of Text

--- src/swarm-scenario/Swarm/Game/Scenario/Style.hs
+++ src/swarm-scenario/Swarm/Game/Scenario/Style.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ViewPatterns #-}
+
 -- |
 -- SPDX-License-Identifier: BSD-3-Clause
 --
@@ -7,9 +9,9 @@ module Swarm.Game.Scenario.Style where
 import Codec.Picture (PixelRGBA8 (..))
 import Data.Aeson
 import Data.Colour.Palette.Types (Kolor)
-import Data.Colour.SRGB (RGB (..), sRGB24read, toSRGB24)
+import Data.Colour.SRGB (RGB (..), sRGB24reads, toSRGB24, sRGB24show)
+import Data.Colour.SRGB.Linear (toRGB)
 import Data.Set (Set)
-import Data.Text (Text)
 import Data.Text qualified as T
 import GHC.Generics (Generic)
 import Swarm.Game.Entity.Cosmetic
@@ -38,17 +40,36 @@ instance FromJSON StyleFlag where
 instance ToJSON StyleFlag where
   toJSON = genericToJSON styleFlagJsonOptions
 
--- | Hexadecimal color notation.
--- May include a leading hash symbol (see 'Data.Colour.SRGB.sRGB24read').
-newtype HexColor = HexColor Text
-  deriving (Eq, Ord, Show, Generic, FromJSON, ToJSON)
+-- | A color, parsed from hexadecimal notation.  May include a leading
+--   hash symbol (see 'Data.Colour.SRGB.sRGB24read').
+newtype HexColor = HexColor { getHexColor :: Kolor }
+  deriving (Eq, Show, Generic)
+
+instance Ord HexColor where
+  -- There is no Ord instance for Colour a, but we need one to use
+  -- with OccurrenceEncoder, so we make our own.
+  --
+  -- We use toRGB here since it does no conversions whatsoever, it
+  -- simply unpacks the raw color data into an RGB triple.  For the
+  -- purposes of an Ord instance, it doesn't matter: we just want a
+  -- consistent way to put a total ordering on colors as fast as
+  -- possible.
+  compare (HexColor (toRGB -> RGB r1 g1 b1)) (HexColor (toRGB -> RGB r2 g2 b2))
+    = compare r1 r2 <> compare g1 g2 <> compare b1 b2
+
+instance FromJSON HexColor where
+  parseJSON = withText "hex color" $ \t ->
+    case sRGB24reads (T.unpack t) of
+      ((c, _):_) -> pure $ HexColor c
+      _ -> fail $ "Could not parse hex color '" ++ T.unpack t ++ "'"
+
+instance ToJSON HexColor where
+  toJSON = toJSON . T.pack . sRGB24show . getHexColor
 
 instance ToPixel HexColor where
-  toPixel (HexColor colorText) = PixelRGBA8 r g b 255
+  toPixel (HexColor kolor) = PixelRGBA8 r g b 255
    where
-    temp :: Kolor
-    temp = sRGB24read $ T.unpack colorText
-    RGB r g b = toSRGB24 temp
+    RGB r g b = toSRGB24 kolor
 
 data CustomAttr = CustomAttr
   { name :: String
@@ -77,7 +98,4 @@ toHifiPair (CustomAttr n maybeFg maybeBg _) =
     (Nothing, Just b) -> Just $ BgOnly b
     (Nothing, Nothing) -> Nothing
 
-  conv (HexColor x) = Triple $ toSRGB24 kolor
-   where
-    kolor :: Kolor
-    kolor = sRGB24read $ T.unpack x
+  conv (HexColor kolor) = Triple $ toSRGB24 kolor
