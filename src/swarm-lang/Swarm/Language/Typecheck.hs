fc2ad2d9478e85562f860ceeb75d6846e777c8d5
Brent Yorgey <byorgey@gmail.com>
2025-01-15 17:56:01 -0600

    typo fix

--- src/swarm-lang/Swarm/Language/Typecheck.hs
+++ src/swarm-lang/Swarm/Language/Typecheck.hs
@@ -617,7 +617,7 @@ data InvalidAtomicReason
     LongConst
   | -- | The argument contained a suspend
     AtomicSuspend
-  | -- | The argument container an import
+  | -- | The argument contained an import
     AtomicImport
   deriving (Show)
 
