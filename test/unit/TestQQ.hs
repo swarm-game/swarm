831f9516dae328276c327a65d68f9942a3849c18
Brent Yorgey <byorgey@gmail.com>
2025-04-25 17:06:04 -0500

    add quasiquoter tests

---
+++ test/unit/TestQQ.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+-- |
+-- SPDX-License-Identifier: BSD-3-Clause
+--
+-- Unit tests for quasiquoting
+module TestQQ where
+
+import Swarm.Language.Parser.QQ
+import Swarm.Language.Syntax
+import Swarm.Pretty
+import Test.Tasty
+import Test.Tasty.HUnit
+
+testQQ :: TestTree
+testQQ =
+  testGroup
+    "quasiquoters"
+    [ testCase "basic untyped AST quasiquote"
+      $ assertEqual "numeric literal"
+          [astQ| 3 |]
+          (Syntax' (SrcLoc 1 2) (TInt 3) mempty ())
+    , testCase "antiquoting for untyped AST quasiquotes"
+      $ let t = [astQ| 3 + 6 |]
+        in  assertEqual "foo"
+              (prettyText [astQ| def x : Int -> Cmd Bool = \x. pure (x == `t) end |])
+              (prettyText [astQ| def x : Int -> Cmd Bool = \x. pure (x == 3 + 6) end |])
+              -- Note, they are not equal as ASTs because the SrcLoc
+              -- annotations differ.
+    ]
