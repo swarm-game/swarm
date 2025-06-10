59a19a7366ccf57b63432b64df03c44d1a40f017
Brent Yorgey <byorgey@gmail.com>
2025-06-10 09:18:33 +0200

    LSP fixes; move SrcLoc to swarm-util

--- src/swarm-lang/Swarm/Language/Var.hs
+++ src/swarm-lang/Swarm/Language/Var.hs
@@ -5,8 +5,13 @@
 module Swarm.Language.Var where
 
 import Data.Text (Text)
+import Swarm.Util.SrcLoc (Located)
 
 -- | For now, we just use 'Text' to represent variables.  In theory,
 --   at some point in the future we might want to represent them in some
 --   fancier way.
 type Var = Text
+
+-- | A convenient type synonym for variables with an associated
+-- SrcLoc.
+type LocVar = Located Var
