507828191060292a21b65ebffbe18c3544c7cb92
Brent Yorgey <byorgey@gmail.com>
2025-06-16 23:06:32 +0200

    add Swarm.Game.Cosmetic module for re-exports

---
+++ src/swarm-scenario/Swarm/Game/Cosmetic.hs
@@ -0,0 +1,19 @@
+-- |
+-- SPDX-License-Identifier: BSD-3-Clause
+--
+-- Code having to do with cosmetic aspects of the world display and
+-- how it is constructed (colors, attributes, texels, etc.).
+module Swarm.Game.Cosmetic
+  ( module Swarm.Game.Cosmetic.Assignment
+  , module Swarm.Game.Cosmetic.Attribute
+  , module Swarm.Game.Cosmetic.Color
+  , module Swarm.Game.Cosmetic.Display
+  , module Swarm.Game.Cosmetic.Texel
+  )
+where
+
+import Swarm.Game.Cosmetic.Assignment
+import Swarm.Game.Cosmetic.Attribute
+import Swarm.Game.Cosmetic.Color
+import Swarm.Game.Cosmetic.Display
+import Swarm.Game.Cosmetic.Texel
