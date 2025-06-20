793bf53fece52b1a1db4bb78d5d9e840d18abfe4
oliverpauffley <mrpauffley@gmail.com>
2025-06-20 11:59:51 +0100

    test: add test for narrowToPosition

--- test/unit/TestLSP.hs
+++ test/unit/TestLSP.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 -- |
 -- SPDX-License-Identifier: BSD-3-Clause
@@ -6,11 +7,16 @@
 -- LSP unit tests
 module TestLSP (testLSP) where
 
+import Data.List.NonEmpty (NonEmpty (..))
 import Data.Text (Text)
 import Data.Text.IO qualified as TIO
+import Debug.Trace
+import Swarm.Language.LSP.Hover (narrowToPosition)
 import Swarm.Language.LSP.VarUsage qualified as VU
 import Swarm.Language.Parser (readTerm)
+import Swarm.Language.Parser.QQ
 import Swarm.Language.Syntax qualified as S
+import Swarm.Pretty (PrettyPrec (prettyPrec), ppr)
 import System.FilePath ((</>))
 import Test.Tasty
 import Test.Tasty.HUnit
@@ -27,67 +33,84 @@ simplifyWarning (VU.VarUsage (S.LV _ v) scope) = UnusedVar v scope
 testLSP :: TestTree
 testLSP =
   testGroup
-    "Unused variable warnings"
-    [ testCase "outer lambda"
-        . checkFile "multiple-lambda-first-unused.sw"
-        . pure
-        $ UnusedVar "x" VU.Lambda
-    , testCase "inner lambda"
-        . checkFile "multiple-lambda-second-unused.sw"
-        . pure
-        $ UnusedVar "y" VU.Lambda
-    , testCase "shadowed variable name"
-        . checkFile "shadowed-variable-lambda-unused.sw"
-        . pure
-        $ UnusedVar "x" VU.Lambda
-    , testCase "outer let"
-        . checkFile "multiple-let-first-unused.sw"
-        . pure
-        $ UnusedVar "x" VU.Let
-    , testCase "outer let"
-        . checkFile "multiple-let-second-unused.sw"
-        . pure
-        $ UnusedVar "y" VU.Let
-    , testCase "multiple unused let" $
-        checkFile
-          "multiple-let-all-unused.sw"
-          [UnusedVar "x" VU.Let, UnusedVar "y" VU.Let]
-    , testCase "shadowed let without usage" $
-        checkFile
-          "shadowed-variable-let-unused.sw"
-          [UnusedVar "x" VU.Let]
-    , testCase "shadowed let with intermediate usage" $
-        checkFile
-          "shadowed-variable-let-intermediate-use.sw"
-          []
-    , testCase "recursive let" $
-        checkFile
-          "recursive-let.sw"
-          [UnusedVar "fac" VU.Let]
-    , testCase "single unused bind" $
-        checkFile
-          "single-bind-unused.sw"
-          [UnusedVar "x" VU.Bind]
-    , testCase "single used bind" $
-        checkFile
-          "single-bind-used.sw"
-          []
-    , testCase "lambda with var used inside annotation" $
-        checkFile
-          "lambda-with-annot.sw"
-          []
-    , testCase "record with used var" $
-        checkFile
-          "lambda-with-record-used.sw"
-          []
-    , testCase "record with used var abbrev" $
-        checkFile
-          "lambda-with-record-used-abbrev.sw"
-          []
-    , testCase "record with unused var" $
-        checkFile
-          "lambda-with-record-unused.sw"
-          [UnusedVar "y" VU.Lambda]
+    "Test LSP"
+    [ testGroup
+        "Unused variable warnings"
+        [ testCase "outer lambda"
+            . checkFile "multiple-lambda-first-unused.sw"
+            . pure
+            $ UnusedVar "x" VU.Lambda
+        , testCase "inner lambda"
+            . checkFile "multiple-lambda-second-unused.sw"
+            . pure
+            $ UnusedVar "y" VU.Lambda
+        , testCase "shadowed variable name"
+            . checkFile "shadowed-variable-lambda-unused.sw"
+            . pure
+            $ UnusedVar "x" VU.Lambda
+        , testCase "outer let"
+            . checkFile "multiple-let-first-unused.sw"
+            . pure
+            $ UnusedVar "x" VU.Let
+        , testCase "outer let"
+            . checkFile "multiple-let-second-unused.sw"
+            . pure
+            $ UnusedVar "y" VU.Let
+        , testCase "multiple unused let" $
+            checkFile
+              "multiple-let-all-unused.sw"
+              [UnusedVar "x" VU.Let, UnusedVar "y" VU.Let]
+        , testCase "shadowed let without usage" $
+            checkFile
+              "shadowed-variable-let-unused.sw"
+              [UnusedVar "x" VU.Let]
+        , testCase "shadowed let with intermediate usage" $
+            checkFile
+              "shadowed-variable-let-intermediate-use.sw"
+              []
+        , testCase "recursive let" $
+            checkFile
+              "recursive-let.sw"
+              [UnusedVar "fac" VU.Let]
+        , testCase "single unused bind" $
+            checkFile
+              "single-bind-unused.sw"
+              [UnusedVar "x" VU.Bind]
+        , testCase "single used bind" $
+            checkFile
+              "single-bind-used.sw"
+              []
+        , testCase "lambda with var used inside annotation" $
+            checkFile
+              "lambda-with-annot.sw"
+              []
+        , testCase "record with used var" $
+            checkFile
+              "lambda-with-record-used.sw"
+              []
+        , testCase "record with used var abbrev" $
+            checkFile
+              "lambda-with-record-used-abbrev.sw"
+              []
+        , testCase "record with unused var" $
+            checkFile
+              "lambda-with-record-unused.sw"
+              [UnusedVar "y" VU.Lambda]
+        ]
+    , testGroup
+        "narrowToPosition"
+        [ testCase "" $
+            assertEqual
+              ""
+              (S.Syntax' (S.SrcLoc 1 52) (S.TInt 3) (S.Comments mempty mempty) ())
+              ( narrowTrace
+                  [astQ|def m2 = move; move end
+                        def m4 = m2; m2 end
+                        def m8 = m4; m4 end
+                        def m16 = m8; m8 end|]
+                  98
+              )
+        ]
     ]
  where
   checkFile :: FilePath -> [UnusedVar] -> IO ()
@@ -105,3 +128,7 @@ testLSP =
        where
         VU.Usage _ problems = VU.getUsage mempty term
       _ -> []
+
+narrowTrace i n =
+  let g = narrowToPosition i n
+   in traceShow (ppr g) g
