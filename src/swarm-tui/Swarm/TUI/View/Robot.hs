0504519607a3ea8443d8f6be91dd759d1b7f9004
Brent Yorgey <byorgey@gmail.com>
2025-06-26 10:13:11 -0500

    Add comments to `RobotColumn` constructors (#2503)
    Closes #2301 .

--- src/swarm-tui/Swarm/TUI/View/Robot.hs
+++ src/swarm-tui/Swarm/TUI/View/Robot.hs
@@ -69,16 +69,32 @@ NEW GRID LIST
 
 -- | The columns in the Robot modal grid.
 data RobotColumn
-  = ColName
-  | ColAge
-  | ColPos
-  | ColItems
-  | ColStatus
-  | ColActns
-  | ColCmds
-  | ColCycles
-  | ColActivity
-  | ColLog
+  = -- | An image of the robot (including its current direction, if
+    --   its display changes with direction), along with its name.
+    ColName
+  | -- | The age of the robot, measured in seconds.
+    ColAge
+  | -- | The position (coordinates) of the robot, along with a visual
+    --   indication of what the robot's current cell looks like.
+    ColPos
+  | -- | The number of items in the robot's inventory (not counting equipped devices).
+    ColItems
+  | -- | The robot's status: busy (actively executing commands),
+    --   waiting (executing a `wait`), or idle
+    ColStatus
+  | -- | The total count of tangible commands executed so far.  See the
+    --   documentation of
+    --   'Swarm.Language.Syntax.Constants.Tangibility'.
+    ColActns
+  | -- | The total count of all commands executed so far.
+    ColCmds
+  | -- | The number of CESK machine steps executed so far.
+    ColCycles
+  | -- | Percentage of the most recent 64 ticks during which the robot
+    --   was busy.
+    ColActivity
+  | -- | Whether the robot has generated any additional log entries.
+    ColLog
   | -- | The ID is the only optional field.
     --   It is shown last to make indexing code easier.
     ColID
