69bae076405b4fa66cd38aa8251edfed070b1740
Ondřej Šebek <ondras98@icloud.com>
2025-08-25 18:52:00 +0200

    Get syntax from import struct

--- src/swarm-doc/Swarm/Doc/Pedagogy.hs
+++ src/swarm-doc/Swarm/Doc/Pedagogy.hs
@@ -56,6 +56,7 @@ import Swarm.Game.ScenarioInfo (
   scenarioCollectionToList,
  )
 import Swarm.Game.World.Load (loadWorlds)
+import Swarm.Language.Load (getSyntax)
 import Swarm.Language.Syntax
 import Swarm.Language.Text.Markdown (docToText, findCode)
 import Swarm.Util.Effect (ignoreWarnings)
@@ -100,7 +101,7 @@ extractCommandUsages :: Int -> ScenarioWith ScenarioPath -> TutorialInfo
 extractCommandUsages idx siPair@(ScenarioWith s _si) =
   TutorialInfo siPair idx solnCommands $ getDescCommands s
  where
-  solnCommands = getCommands maybeSoln
+  solnCommands = getCommands . fmap getSyntax $ maybeSoln
   maybeSoln = view (scenarioOperation . scenarioSolution) s
 
 -- | Obtain the set of all commands mentioned by
