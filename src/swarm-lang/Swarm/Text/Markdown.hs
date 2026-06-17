ac4e725ad166e18b8e33d944cca89268738beef6
Brent Yorgey <byorgey@gmail.com>
2026-06-17 15:32:03 -0500

    more high-level explanation of Markdown processing pipeline

--- src/swarm-lang/Swarm/Text/Markdown.hs
+++ src/swarm-lang/Swarm/Text/Markdown.hs
@@ -1,6 +1,3 @@
--- XXX move to Swarm.Text.Markdown ?
--- XXX write a longer explanatory note here with overview
-
 -- |
 -- SPDX-License-Identifier: BSD-3-Clause
 --
@@ -12,13 +9,24 @@
 --
 -- Overall, the Markdown processing pipeline looks like this:
 --
---   1. Parse Markdown content, using the Commonmark package, into our
---      custom 'Document' data type.  At this point any code blocks
---      contained in the Markdown document are just blobs of text.
+--   1. Parse Markdown content ("Swarm.Text.Markdown.Parse"), using
+--      the Commonmark package, into our custom 'Document' data type
+--      ("Swarm.Text.Markdown.Document").  At this point any code
+--      blocks contained in the Markdown document are just blobs of
+--      text.
 --
 --   2. Run Swarm parsing + typechecking to validate code blocks or
---      inline Swarm code contained in the document, and replace the
---      text blobs with actual ASTs.
+--      inline Swarm code contained in the document
+--      ("Swarm.Text.Markdown.Parse"), and replace the text blobs with
+--      actual ASTs.
+--
+--   3. Turn the document into a stream of tokens
+--      ("Swarm.Text.Markdown.Token"), performing pretty-printing for
+--      code blocks and flowing paragraphs to a specified line
+--      width ("Swarm.Text.Markdown.Layout").
+--
+--   4. The resulting token stream can be easily rendered to text
+--      ("Swarm.Text.Markdown.Render") or to brick widgets.
 module Swarm.Text.Markdown (
   -- * Markdown document model
   Document (..),
