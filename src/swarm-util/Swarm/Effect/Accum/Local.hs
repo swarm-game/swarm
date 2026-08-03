14f1504f5d3e68db99037b093184c8e5d2298eff
Patrick Estrada <212604433+patrickestrada@users.noreply.github.com>
2026-08-03 16:52:27 -0400

    Add thread-local Accum effect

---
+++ src/swarm-util/Swarm/Effect/Accum/Local.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE TypeFamilies #-}
+
+-- |
+-- SPDX-License-Identifier: BSD-3-Clause
+-- Description: Accum effect. The accumulator is thread-local.
+module Swarm.Effect.Accum.Local where
+
+import Data.Kind (Type)
+import Effectful
+import Effectful.Dispatch.Static
+
+data Accum (w :: Type) :: Effect
+type instance DispatchOf (Accum w) = Static NoSideEffects
+newtype instance StaticRep (Accum w) = Accum w
+
+runAccum :: (HasCallStack, Monoid w) => Eff (Accum w : es) a -> Eff es (w, a)
+runAccum m = do
+  (a, Accum w) <- runStaticRep (Accum mempty) m
+  pure (w, a)
+
+add :: (HasCallStack, Accum w :> es, Monoid w) => w -> Eff es ()
+add w = stateStaticRep $ \(Accum w0) -> ((), Accum (w0 <> w))
+
+look :: (HasCallStack, Accum w :> es, Monoid w) => Eff es w
+look = do
+  Accum w <- getStaticRep
+  pure w
+
+looks :: (HasCallStack, Accum w :> es, Monoid w) => (w -> a) -> Eff es a
+looks f = f <$> look
