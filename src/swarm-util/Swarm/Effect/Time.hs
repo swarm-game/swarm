60c1c076c737e63c6b3e64e16470b6950087d9e4
Patrick Estrada <212604433+patrickestrada@users.noreply.github.com>
2026-08-04 20:49:24 -0400

    Fix implementation of Time effect

--- src/swarm-util/Swarm/Effect/Time.hs
+++ src/swarm-util/Swarm/Effect/Time.hs
@@ -24,14 +24,14 @@ import System.Clock (Clock (Monotonic), TimeSpec, getTime, toNanoSecs)
 
 -- | Effect for things related to time
 data Time :: Effect where
-  GetNow' :: Time m TimeSpec
-  GetCpuTime' :: Time m Integer
-  GetZonedTime' :: Time m LT.ZonedTime
+  GetNow :: Time m TimeSpec
+  GetCpuTime :: Time m Integer
+  GetZonedTime :: Time m LT.ZonedTime
 
 type instance DispatchOf Time = Dynamic
 
 getNow :: (HasCallStack, Time :> es) => Eff es TimeSpec
-getNow = send GetNow'
+getNow = send GetNow
 {-# INLINE getNow #-}
 
 measureCpuTimeInSec :: (Time :> es) => Eff es a -> Eff es (Double, a)
@@ -43,19 +43,19 @@ measureCpuTimeInSec f = do
   pure (elapsedSec, res)
 {-# INLINE measureCpuTimeInSec #-}
 
-getZonedTime :: (HasCallStack, Time :> es) => Eff es Integer
-getZonedTime = send GetCpuTime'
+getZonedTime :: (HasCallStack, Time :> es) => Eff es LT.ZonedTime
+getZonedTime = send GetZonedTime
 {-# INLINE getZonedTime #-}
 
 runTimeIO :: (IOE :> es) => Eff (Time : es) a -> Eff es a
 runTimeIO = interpret $ \_ -> \case
-  GetNow' -> liftIO (System.Clock.getTime System.Clock.Monotonic)
-  GetCpuTime' -> liftIO System.CPUTime.getCPUTime
-  GetZonedTime' -> liftIO LT.getZonedTime
+  GetNow -> liftIO (System.Clock.getTime System.Clock.Monotonic)
+  GetCpuTime -> liftIO System.CPUTime.getCPUTime
+  GetZonedTime -> liftIO LT.getZonedTime
 {-# INLINE runTimeIO #-}
 
 runFakeTime :: TimeSpec -> LT.ZonedTime -> Eff (Time : es) a -> Eff es a
 runFakeTime t zt = interpret $ \_ -> \case
-  GetNow' -> pure t
-  GetCpuTime' -> pure (1000 * toNanoSecs t)
-  GetZonedTime' -> pure zt
+  GetNow -> pure t
+  GetCpuTime -> pure (1000 * toNanoSecs t)
+  GetZonedTime -> pure zt
