cc9dd133317633cff50bf89f57d4b0870672d155
Patrick Estrada <212604433+patrickestrada@users.noreply.github.com>
2026-08-07 06:44:24 -0400

    Use custom label type for Warn effect
    We use an empty data type WarnLabel as the label used for the Warn type
    synonym. We don't export it so that the only way to run a Warn effect is
    through the provided handlers.

--- src/swarm-util/Swarm/Effect/Warn/Local.hs
+++ src/swarm-util/Swarm/Effect/Warn/Local.hs
@@ -11,16 +11,18 @@ import Effectful
 import Effectful.Labeled
 import Swarm.Effect.Output.Local
 
+data WarnLabel
+
 -- | The Warn effect
-type Warn w = Labeled "Warning" (Output w)
+type Warn w = Labeled WarnLabel (Output w)
 
 -- | Log a single failure as a warning.
 warn :: forall w es. (Warn w :> es) => w -> Eff es () -- (Warn w :> es) => w -> Eff es ()
-warn w = labeled @"Warning" @(Output w) (output w)
+warn w = labeled @WarnLabel @(Output w) (output w)
 
 -- | Run the Warn effect, accumulating all warnings
 runWarn :: forall w es a. Eff (Warn w : es) a -> Eff es (a, [w])
-runWarn = runLabeled @"Warning" @(Output w) runOutput
+runWarn = runLabeled @WarnLabel @(Output w) runOutput
 
 -- | Run the Warn effect, discarding all warnings
 evalWarn :: forall w es a. Eff (Warn w : es) a -> Eff es a
